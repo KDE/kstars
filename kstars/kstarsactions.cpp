61ca6061605ddd421eba2e1ebf4ba75727851cdf
Jason Harris <kstars@30doradus.org>
2002-04-30 15:22:27 +0000

    joined message string into one long i18n() function, at suggestion of proofreading team (thanks).
    svn path=/trunk/kdeedu/kstars/; revision=152944

--- kstars/kstarsactions.cpp
+++ kstars/kstarsactions.cpp
@@ -184,10 +184,7 @@ void KStars::slotPrint() {
 
 //Suggest Chart color scheme
 	if ( options()->colorSky != "#FFFFFF" ) {
-		QString message = i18n( "You can save printer ink by using the \"Star Chart\" " );
-		message += i18n( "color scheme, which uses a white background.  Would you like " );
-		message += i18n( "to switch to the Star Chart color scheme for printing? " );
-		message += i18n( "(your current color settings will be restored when printing is finished)" );
+		QString message = i18n( "You can save printer ink by using the \"Star Chart\" color scheme, which uses a white background.  Would you like to switch to the Star Chart color scheme for printing? (your current color settings will be restored when printing is finished)" );
 
 //KDE3 version of the messagebox allows us to include a "Don't ask again" checkbox...
 		int answer;
