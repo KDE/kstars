92790f7579877e77cc6a06e60631cc82af5b60d1
Jasem Mutlaq <mutlaqja@ikarustech.com>
2004-03-02 11:47:48 +0000

    Make it compile on Solaris
    svn path=/branches/KDE_3_2_BRANCH/kdeedu/kstars/; revision=293078

--- kstars/indi/indicom.c
+++ kstars/indi/indicom.c
@@ -36,6 +36,9 @@
 const char * Direction[] = { "North", "West", "East", "South", "All"};
 const char * SolarSystem[] = { "Mercury", "Venus", "Moon", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"};
 
+/* make it compile on solaris */
+#define M_PI 3.4159265358979323846264338327950288419716939937510582097494459 
+
 /******** Prototypes ***********/
 double DegToRad( double num );
 double RadToDeg( double num );
