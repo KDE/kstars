1ce17b1f101f282059ef6b6dc306d410311ea8d7
Jason Harris <kstars@30doradus.org>
2004-10-26 03:58:49 +0000

    Added copyright notices to some files.
    CCMAIL: annma@kde.org
    CCMAIL: mutlaqja@ikarustech.com
    
    svn path=/trunk/kdeedu/kstars/; revision=357791

--- kstars/toggleaction.cpp
+++ kstars/toggleaction.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                          toggleaction.cpp  -  description
+                             -------------------
+    begin                : Son Feb 10 2002
+    copyright            : (C) 2002 by Mark Hollomon
+    email                : mhh@mindspring.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "toggleaction.h"
 
 ToggleAction::ToggleAction(const QString& ontext, const QIconSet& onpix,
