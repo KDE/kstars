eacbeff951590c94eed242393637f906e857b163
Jason Harris <kstars@30doradus.org>
2005-09-30 14:41:53 +0000

    Update version for 3.5-beta2
    svn path=/branches/KDE/3.5/kdeedu/kstars/; revision=465698

--- kstars/main.cpp
+++ kstars/main.cpp
@@ -29,7 +29,7 @@
 #include "ksnumbers.h"
 #include "Options.h"
 
-#define KSTARS_VERSION "1.1.90"
+#define KSTARS_VERSION "1.1.92"
 
 static const char description[] =
 	I18N_NOOP("Desktop Planetarium");
