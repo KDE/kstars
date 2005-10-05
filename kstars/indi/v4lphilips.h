2c7ae826f0eb4f0f9e7b8f9ba49966c720be0407
Dirk Mueller <mueller@kde.org>
2005-10-05 09:03:00 +0000

    SVN_SILENT: fix typo
    svn path=/trunk/KDE/kdeedu/kstars/; revision=467420

--- kstars/indi/v4lphilips.h
+++ kstars/indi/v4lphilips.h
@@ -14,7 +14,7 @@
 
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301  USA
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
     2005.04.29  JM: There is no need for this file for Video 4 Linux 2. It is kept for V4L 1 compatiblity.
 
