6fb407f4c28390168108d65c63fcd85b5571f8ee
Jason Harris <kstars@30doradus.org>
2005-11-12 17:25:20 +0000

    oops, forgot to add the new class MilkyWayComposite
    CCMAIL: kstars-devel@kde.org
    
    svn path=/trunk/KDE/kdeedu/kstars/; revision=479991

---
+++ kstars/skycomponents/milkywaycomposite.cpp
@@ -0,0 +1,32 @@
+/***************************************************************************
+                          milkywaycomposite.cpp  -  K Desktop Planetarium
+                             -------------------
+    begin                : 12 Nov. 2005
+    copyright            : (C) 2005 by Jason Harris
+    email                : kstars@30doradus.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "milkywaycomposite.h"
+#include "milkywaycomponent.h"
+
+MilkyWayComposite::MilkyWayComposite( SkyComponent *parent, bool (*visibleMethod)() ) 
+	: SkyComposite(parent) 
+{
+	for ( uint i=1; i <= NMWFILES; ++i ) {
+		QString fname;
+		if ( i < 10 ) fname = "mw0" + QString().setNum(i) + ".dat";
+		else          fname = "mw" + QString().setNum(i) + ".dat";
+
+		addComponent( new MilkyWayComponent( this, fname, visibleMethod ) );
+	}
+}
+
---
+++ kstars/skycomponents/milkywaycomposite.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+                          milkywaycomposite.h  -  K Desktop Planetarium
+                             -------------------
+    begin                : 2005/07/08
+    copyright            : (C) 2005 by Jason Harris
+    email                : kstars@30doradus.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef MILKYWAYCOMPOSITE_H
+#define MILKYWAYCOMPOSITE_H
+
+#define NMWFILES  11
+
+#include "skycomposite.h"
+
+/**
+	*@class MlkyWayComposite
+	*The Milky Way is stored in 11 separate "chunks".
+	*This composite stores a MilkyWayComponent for each chunk.
+	*
+	*@author Jason Harris
+	*@version 0.1
+	*/
+class MilkyWayComposite : public SkyComposite
+{
+	public:
+
+		/**
+			*@short Constructor
+			*@p parent pointer to the parent SkyComponent
+			*/
+		MilkyWayComposite( SkyComponent *parent, bool (*visibleMethod)() );
+
+};
+
+#endif
