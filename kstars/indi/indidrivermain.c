d4ca4258f6a097281d07c09527e47d2074f1ef9b
Dirk Mueller <mueller@kde.org>
2006-05-03 21:30:36 +0000

    fix initial allocations (CID 1779)
    svn path=/trunk/KDE/kdeedu/kstars/; revision=537095

--- kstars/indi/indidrivermain.c
+++ kstars/indi/indidrivermain.c
@@ -1101,10 +1101,10 @@ dispatch (XMLEle *root, char msg[])
 
 	    /* seed for reallocs */
 	    if (!blobs) {
-		blobs = (char **) malloc (1);
-		names = (char **) malloc (1);
-		formats = (char **) malloc (1);
-		blobsizes = (int *) malloc (1);
+		blobs = (char **) malloc (sizeof(void*));
+		names = (char **) malloc (sizeof(void*));
+		formats = (char **) malloc (sizeof(void*));
+		blobsizes = (int *) malloc (sizeof(void*));
 	    }
 
 	    /* pull out each name/BLOB pair */
