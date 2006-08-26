973edc8290a90562412dead54c24562dca98953a
Stephan Kulow <coolo@kde.org>
2006-08-26 13:23:32 +0000

    looks like this header is not needed (and does not exist on ppc)
    svn path=/trunk/KDE/kdeedu/kstars/; revision=577372

--- kstars/indi/apogee/CameraIO_Linux.cpp
+++ kstars/indi/apogee/CameraIO_Linux.cpp
@@ -22,7 +22,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 //////////////////////////////////////////////////////////////////////
 
 #include <assert.h>
-#include <sys/io.h>  
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <sys/ioctl.h>
