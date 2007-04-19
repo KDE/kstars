b3dfb5e62dbd61039fb2ab7418e84de8ebc05bef
Laurent Montel <montel@kde.org>
2007-04-19 12:48:13 +0000

    Fix compile
    svn path=/trunk/KDE/kdeedu/kstars/; revision=655817

--- kstars/indi/celestronprotocol.c
+++ kstars/indi/celestronprotocol.c
@@ -35,12 +35,12 @@
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
 #ifndef _WIN32
 #include <termios.h>
 #endif
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
 #endif
 
 #include <math.h>
