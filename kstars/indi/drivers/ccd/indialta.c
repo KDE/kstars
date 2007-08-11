6d8b3813bf08cf865d738052bf75f2cd6a1c245f
Jasem Mutlaq <mutlaqja@ikarustech.com>
2007-08-11 23:39:25 +0000

    include headers to compile vanilla INDI
    svn path=/trunk/KDE/kdeedu/kstars/; revision=699078

--- kstars/indi/drivers/ccd/indialta.c
+++ kstars/indi/drivers/ccd/indialta.c
@@ -21,20 +21,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdarg.h>
-#include <dirent.h>
 #include <math.h>
 #include <errno.h>
 #include <ctype.h>
 #include <fcntl.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 
 #include <fitsio.h>
 
