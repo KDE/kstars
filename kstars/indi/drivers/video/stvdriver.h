07dee2057a71ae80db985fda9bfbe464e5cd9fe4
Dirk Mueller <mueller@kde.org>
2007-11-08 22:49:41 +0000

    fix FSF address
    svn path=/trunk/KDE/kdeedu/kstars/; revision=734468

--- kstars/indi/drivers/video/stvdriver.h
+++ kstars/indi/drivers/video/stvdriver.h
@@ -14,7 +14,7 @@
 
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301  USA
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 
 #define OFF 0 
@@ -158,7 +158,7 @@ typedef struct {
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
    Or visit http://www.gnu.org/licenses/gpl.html.
 */
