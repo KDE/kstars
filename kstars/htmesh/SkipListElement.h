f59de0bfbb49466c80f21f1aff080412ff2e14be
Marc Espie <espie@nerim.net>
2008-01-28 10:12:04 +0000

    compile on OpenBSD
    svn path=/trunk/KDE/kdeedu/kstars/; revision=767547

--- kstars/htmesh/SkipListElement.h
+++ kstars/htmesh/SkipListElement.h
@@ -24,7 +24,7 @@
 #ifdef _WIN32
 #define KEY_MAX _I64_MAX
 #else
-#  if defined(__FreeBSD__) || defined(__NetBSD__)
+#  if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
 #    define KEY_MAX LLONG_MAX
 #  else
 #    define KEY_MAX LONG_LONG_MAX
