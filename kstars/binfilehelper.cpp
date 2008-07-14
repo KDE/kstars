8c2b52d9542bc77269b035b48ef08129e7222729
Akarsh Simha <akarshsimha@gmail.com>
2008-07-14 20:04:34 +0000

    + Add missing copyright notice in binfilehelper.cpp
    + Include own header first
    
    CCMAIL: kstars-devel@kde.org
    
    
    svn path=/trunk/KDE/kdeedu/kstars/; revision=832483

--- kstars/binfilehelper.cpp
+++ kstars/binfilehelper.cpp
@@ -1,6 +1,24 @@
-#include <kstandarddirs.h>
+
+/***************************************************************************
+                   binfilehelper.cpp - K Desktop Planetarium
+                             -------------------
+    begin                : Sat May 31 2008
+    copyright            : (C) 2008 by Akarsh Simha
+    email                : akarshsimha@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "binfilehelper.h"
+
+#include <kstandarddirs.h>
 #include "byteswap.h"
 
 class BinFileHelper;
