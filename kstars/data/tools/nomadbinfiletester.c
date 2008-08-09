28265be548d1dfc5d67c99c1f6a0f6a20be61e7a
Akarsh Simha <akarshsimha@gmail.com>
2008-08-09 08:29:08 +0000

    Adding the machinery required to produce binary data files for the USNO NOMAD Catalog.
    CCMAIL: kstars-devel@kde.org
    
    
    svn path=/trunk/KDE/kdeedu/kstars/; revision=844300

---
+++ kstars/data/tools/nomadbinfiletester.c
@@ -0,0 +1,349 @@
+
+/***************************************************************************
+     nomadbinfiletester.c - Check star catalog (binary) files for errors
+                             -------------------
+    begin                : Tue Jul 15 2008
+    copyright            : (C) 2008 by Akarsh Simha
+    email                : akarshsimha@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#define HTM_LEVEL 6
+#define INDEX_ENTRY_SIZE 12
+
+#include "binfile.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/types.h>
+#include <string.h>
+#include "byteswap.h"
+
+
+
+/*
+ * struct to store star data, to be written in this format, into the binary file.
+ */
+
+typedef struct deepStarData {
+    int32_t RA;
+    int32_t Dec;
+    int16_t dRA;
+    int16_t dDec;
+    int16_t B;
+    int16_t V;
+} deepStarData;
+
+dataElement de[100];
+u_int16_t nfields;
+long index_offset, data_offset;
+char byteswap;
+u_int32_t ntrixels;
+
+/*
+ * Does byteswapping for deepStarData structures
+ */
+void bswap_stardata( deepStarData *stardata ) {
+    bswap_32( stardata->RA );
+    bswap_32( stardata->Dec );
+    bswap_16( stardata->dRA );
+    bswap_16( stardata->dDec );
+    bswap_16( stardata->B );
+    bswap_16( stardata->V );
+}
+
+
+int verifyIndexValidity(FILE *f) {
+
+    int i;
+    u_int32_t trixel;
+    u_int32_t offset;
+    u_int32_t prev_offset;
+    u_int32_t nrecs;
+    u_int32_t prev_nrecs;
+    unsigned int nerr;
+
+    fprintf(stdout, "Performing Index Table Validity Check...\n");
+    fprintf(stdout, "Assuming that index starts at %X\n", ftell(f));
+    index_offset = ftell(f);
+
+    prev_offset = 0;
+    prev_nrecs = 0;
+    nerr = 0;
+
+    for(i = 0; i < ntrixels; ++i) {
+        if(!fread(&trixel, 4, 1, f)) {
+            fprintf(stderr, "Table truncated before expected! Read i = %d records so far\n", i);
+            +nerr;
+            break;
+        }
+        if( byteswap ) bswap_32( trixel );
+        if(trixel >= ntrixels) {
+            fprintf(stderr, "Trixel number %u is greater than the expected number of trixels %u\n", trixel, ntrixels);
+            ++nerr;
+        }
+        if(trixel != i) {
+
+            fprintf(stderr, "Found trixel number = %d, while I expected number = %d\n", 
+                    trixel, i);
+            ++nerr;
+        }
+        fread(&offset, 4, 1, f);
+        if( byteswap ) bswap_32( offset );
+        fread(&nrecs, 4, 1, f);
+        if( byteswap ) bswap_32( nrecs );
+        if( prev_offset != 0 && prev_nrecs != (-prev_offset + offset)/sizeof(deepStarData) ) { 
+            fprintf( stderr, "Expected %u  = (%X - %x) / 16 records, but found %u, in trixel %d\n", 
+                     (offset - prev_offset) / 16, offset, prev_offset, nrecs, trixel );
+            ++nerr;
+        }
+        prev_offset = offset;
+        prev_nrecs = nrecs;
+    }
+
+    data_offset = ftell(f);
+
+    if(nerr) { fprintf(stderr, "ERROR ;-): The index seems to have %u errors\n", nerr); }
+    else { fprintf(stdout, "Index verified. PASSED.\n"); }
+
+}
+
+/**
+ *This method ensures that the data part of the file is sane. It ensures that there are no jumps in magnitude etc.
+ */
+int verifyStarData( FILE *f ) {
+    int16_t faintMag;
+    int8_t HTM_Level;
+    u_int16_t MSpT;
+    int16_t realFaintMag;
+    u_int16_t realMSpT;
+    u_int32_t nstars;
+    u_int32_t offset;
+
+    int trixel, i;
+    int nerr_trixel;
+    int nerr;
+
+    deepStarData data;
+    int16_t mag;
+
+    fprintf( stdout, "Assuming that the data starts at %ld\n", ftell( f ) );
+    fread( &faintMag, 2, 1, f );
+    fprintf( stdout, "Faint Magnitude Limit: %f\n", faintMag / 1000.0 );
+    fread( &HTM_Level, 1, 1, f );
+    fprintf( stdout, "HTMesh Level: %d\n", HTM_Level );
+    if( HTM_Level != HTM_LEVEL ) {
+        fprintf( stderr, "ERROR: HTMesh Level in file (%d) and HTM_LEVEL in program (%d) differ. Please set the define directive for HTM_LEVEL correctly and rebuild\n.", HTM_Level, HTM_LEVEL );
+        return 0;
+    }
+    fread( &MSpT, 2, 1, f );
+
+    mag = -5000;
+    nerr = 0;
+
+    // Scan the file for magnitude jumps, etc.
+    realMSpT = 0;
+    for( trixel = 0; trixel < ntrixels; ++trixel ) {
+        mag = -5000;
+        nerr_trixel = 0;
+        fprintf( stdout, "Checking trixel #%d: ", trixel );
+        fseek( f, index_offset + trixel * INDEX_ENTRY_SIZE + 4 , SEEK_SET );
+        fread( &offset, 4, 1, f );
+        fread( &nstars, 4, 1, f );
+        if( nstars > realMSpT )
+            realMSpT = nstars;
+        fseek( f, offset, SEEK_SET );
+        for( i = 0; i < nstars; ++i ) {
+            fread( &data, sizeof( deepStarData ), 1, f );
+            double new_mag = ( ( data.V == 30000 ) ? ( data.B - 1600 ) : data.V );
+            if( mag != -5000 && ( ( new_mag - mag ) > 20 && mag < 1250 ) || new_mag < mag ) { // TODO: Make sensible magnitude limit (1250) user specifiable
+                // TODO: Enable byteswapping
+                fprintf( stderr, "\n\tEncountered jump of %f at star #%d in trixel %d from %f to %f.", ( new_mag - mag ) / 1000.0, i, trixel, mag / 1000.0, new_mag / 1000.0 );
+                ++nerr_trixel;
+            }
+            mag = new_mag;
+            if( mag > realFaintMag ) {
+                realFaintMag = mag;
+            }
+        }
+        if( nerr_trixel > 0 )
+            fprintf( stderr, "\n * Encountered %d magnitude jumps in trixel %d\n", nerr_trixel, trixel );
+        else
+            fprintf( stdout, "Successful\n" );
+        nerr += nerr_trixel;
+    }
+    if( MSpT != realMSpT ) {
+        fprintf( stderr, "ERROR: MSpT according to file = %d, but turned out to be %d\n", MSpT, realMSpT );
+        nerr++;
+    }
+    if( realFaintMag != faintMag ) {
+        fprintf( stderr, "ERROR: Faint Magnitude according to file = %f, but turned out to be %f\n", faintMag / 1000.0, realFaintMag / 1000.0 );
+        nerr++;
+    }
+    if( nerr > 0 ) {
+        fprintf( stderr, "ERROR: Exiting with %d errors\n", nerr );
+        return 0;
+    }
+    fprintf( stdout, "Data validation success!\n" );
+    return 1;
+}
+
+void readStarList(FILE *f, int trixel, FILE *names) {
+    int id;
+    long offset;
+    long n;
+    u_int32_t nrecs;
+    u_int32_t trix;
+    char bayerName[8];
+    char longName[32];
+    char str[6];
+    deepStarData data;
+
+    id = trixel;
+    printf("Reading star list for trixel %d\n", id);
+    rewind(f);
+    offset = index_offset + id * 8; // CAUTION: Change if the size of each entry in the index table changes
+    fseek(f, offset, SEEK_SET);
+    fread(&trix, 4, 1, f);
+    if( byteswap ) bswap_32( trix );
+    if(trix != id) {
+        fprintf(stderr, "ERROR: Something fishy in the index. I guessed that %d would be here, but instead I find %d. Aborting.\n", id, trix);
+        return;
+    }
+    fread(&offset, 4, 1, f);
+    if( byteswap ) bswap_32( offset );
+    fread(&nrecs, 4, 1, f);
+    if( byteswap ) bswap_32( nrecs );
+
+    if(fseek(f, offset, SEEK_SET)) {
+        fprintf(stderr, "ERROR: Could not seek to position %X in the file. The file is either truncated or the indexes are bogus.\n", offset);
+        return;
+    }
+    printf("Data for trixel %d starts at offset 0x%X and has %d records\n", trixel, offset, nrecs);
+
+    for(id = 0; id < nrecs; ++id) {
+        offset = ftell(f);
+        n = (offset - data_offset)/0x20;
+        fread(&data, sizeof(deepStarData), 1, f);
+        if( byteswap ) bswap_stardata( &data );
+        printf("Entry #%d\n", id);
+        printf("\tRA = %f\n", data.RA / 1000000.0);
+        printf("\tDec = %f\n", data.Dec / 100000.0);
+        printf("\tdRA/dt = %f\n", data.dRA / 1000.0);
+        printf("\tdDec/dt = %f\n", data.dDec / 1000.0);
+        printf("\tB = %f\n", data.B / 1000.0);
+        printf("\tV = %f\n", data.V / 1000.0);
+        /*
+          if(data.flags & 0x01 && names) {
+          fseek(names, n * (32 + 8) + 0xA0, SEEK_SET);
+          fread(bayerName, 8, 1, names);
+          fread(longName, 32, 1, names);
+          printf("\t\tBayer Designation = %s\n", bayerName);
+          printf("\t\tLong Name = %s\n", longName);
+          }
+        */
+    }
+}
+
+/**
+ *@short  Read the KStars binary file header and display its contents
+ *@param f  Binary file to read from
+ *@returns  non-zero if successful, zero if not
+ */
+
+int readFileHeader(FILE *f) {
+    int i;
+    int16_t endian_id;
+    char ASCII_text[125];
+    u_int8_t version_no;
+
+    if(f == NULL)
+        return 0;
+
+    fread(ASCII_text, 124, 1, f);
+    ASCII_text[124] = '\0';
+    printf("%s", ASCII_text);
+
+    fread(&endian_id, 2, 1, f);
+    if(endian_id != 0x4B53) {
+        fprintf( stdout, "Byteswapping required\n" );
+        byteswap = 1;
+    }
+    else {
+        fprintf( stdout, "Byteswapping not required\n" );
+        byteswap = 0;
+    }
+
+    fread(&version_no, 1, 1, f);
+    fprintf( stdout, "Version number: %d\n", version_no );
+
+    fread(&nfields, 2, 1, f);
+    if( byteswap ) bswap_16( nfields );
+    fprintf( stdout, "%d fields reported\n", nfields );
+
+    for(i = 0; i < nfields; ++i) {
+        fread(&(de[i]), sizeof(struct dataElement), 1, f);
+        if( byteswap ) bswap_32( de->scale );
+        displayDataElementDescription(&(de[i]));
+    }
+
+    fread(&ntrixels, 4, 1, f);
+    if( byteswap ) bswap_32( ntrixels );
+    fprintf( stdout, "Number of trixels reported = %d\n", ntrixels );
+
+    return 1;
+}
+
+
+int main(int argc, char *argv[]) {
+
+    FILE *f, *names;
+    int16_t maglim = -500;
+    names = NULL;
+    if(argc <= 1) {
+        fprintf(stderr, "USAGE: %s filename [trixel]\n", argv[0]);
+        fprintf(stderr, "Designed for use only with KStars star data files\n");
+        return 1;
+    }
+
+    f = fopen(argv[1], "rb");
+
+    if(f == NULL) {
+        fprintf(stderr, "ERROR: Could not open file %s for binary read.\n", argv[1]);
+        return 1;
+    }
+
+    readFileHeader(f);
+
+    verifyIndexValidity(f);
+    verifyStarData(f);
+
+    fread(&maglim, 2, 1, f);
+    fprintf(stdout, "Limiting Magnitude of Catalog File: %f\n", maglim / 1000.0);
+
+    if(argc > 2) {
+        /*
+          if(argc > 3)
+          names = fopen(argv[3], "rb");
+          else
+          names = NULL;
+        
+          fprintf(stderr, "Names = %s\n", ((names)?"Yes":"No"));
+        */
+        readStarList(f, atoi( argv[2] ), names);
+    }
+
+    fclose(f);
+    if(names) fclose(names);
+
+    return 0;
+}
---
+++ kstars/data/tools/nomadmysql2bin.c
@@ -0,0 +1,311 @@
+
+/***************************************************************************
+            mysql2bin.c - Convert data from MySQL to binary format
+                             -------------------
+    begin                : Mon May 26 2008
+    copyright            : (C) 2008 by Akarsh Simha
+    email                : akarshsimha@gmail.com
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// Program to convert star data stored in a MySQL database into 
+// the binary file format used by KStars
+
+
+#define DB_TBL "nomad"
+#define DB_DB "stardb"
+#define VERBOSE 1
+#define HTM_LEVEL 6
+#define NTRIXELS 32768                         // TODO: Change if HTM Level Changes
+#define INDEX_ENTRY_SIZE 12
+#define MYSQL_STARS_PER_QUERY 1000000
+
+#include "binfile.h"
+
+#include <mysql/mysql.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+/*
+ * struct to store star data, to be written in this format, into the binary file.
+ */
+
+typedef struct deepStarData {
+    int32_t RA;
+    int32_t Dec;
+    int16_t dRA;
+    int16_t dDec;
+    int16_t B;
+    int16_t V;
+} deepStarData;
+
+/*
+ * Dump the data file header.
+ *
+ * WARNING: Must edit every time the definition of the deepStarData structures changes
+ *
+ * f : Data file handle
+ */
+
+int writeDataFileHeader(FILE *f) {
+    char ASCII_text[ 124 ];
+    u_int16_t nfields;
+    u_int32_t nindexes;
+    int16_t endian_id=0x4B53;
+    u_int8_t version_no = 1;
+    
+    if(f == NULL)
+      return 0;
+    
+    nfields = 6;
+    
+    str2charv(ASCII_text, "KStars Star Data v1.0. To be read using the 32-bit starData structure only.", 124);
+    fwrite(ASCII_text, 124, 1, f);
+    fwrite(&endian_id, 2, 1, f);
+    fwrite(&version_no, 1, 1, f);
+    fwrite(&nfields, sizeof(u_int16_t), 1, f);
+    
+    
+    writeDataElementDescription(f, "RA", 4, DT_INT32, 1000000);
+    writeDataElementDescription(f, "Dec", 4, DT_INT32, 100000);
+    writeDataElementDescription(f, "dRA", 2, DT_INT16, 10);
+    writeDataElementDescription(f, "dDec", 2, DT_INT16, 10);
+    writeDataElementDescription(f, "B", 2, DT_INT16, 100);
+    writeDataElementDescription(f, "V", 2, DT_INT16, 100);
+    
+    nindexes = NTRIXELS;
+    fwrite(&nindexes, sizeof(u_int32_t), 1, f);
+
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+
+    /* === Declarations === */
+
+    int ret, i, exitflag;
+    long int lim;
+
+    double Mag;
+
+    unsigned long us_header_offset;
+    unsigned long usf_trix_begin;
+    unsigned long usf_trix_count;
+    unsigned long ntrixels;
+    int16_t maglim;
+    u_int8_t htm_level;
+    u_int16_t MSpT_unnamed;
+
+    char query[512];
+    u_int32_t current_trixel;
+
+    /* File streams */
+    FILE *f;                 /* Pointer to "current" file */
+    FILE *usf;               /* Handle to star data file */
+    FILE *usfhead;           /* Handle to star header file */
+
+    /* deepStarData structure */
+    deepStarData data;
+
+    /* MySQL structures */
+    MYSQL link;
+    MYSQL_RES *result;
+    MYSQL_ROW row;
+
+    /* Check the number of arguments */
+    if( argc <= 6 ) {
+        fprintf(stderr, 
+                "USAGE %s DBUserName DBPassword DeepStarDataFile DeepStarHeaderFile DBName [TableName]\n", 
+                argv[0]);
+        fprintf(stderr, "The database used is a MySQL DB on localhost. The default table name is `nomad`\n");
+    }
+
+    /* == Open all file streams required == */
+    /* Unnamed Star Handling */
+    usf = fopen(argv[3], "wb");
+    if(usf == NULL) {
+        fprintf(stderr, "ERROR: Could not open %s [Deep Star Data File] for binary write.\n", argv[3]);
+        return 1;
+    }
+
+    usfhead = fopen(argv[4], "wb");
+    if(usfhead == NULL) {
+        fprintf(stderr, "ERROR: Could not open %s [Deep Star Header File] for binary write.\n", argv[4]);
+        fcloseall();
+        return 1;
+    }
+
+    /* MySQL connect */
+    if(mysql_init(&link) == NULL) {
+        fprintf(stderr, "ERROR: Failed to initialize MySQL connection!\n");
+        return 1;
+    }
+
+    ret = mysql_real_connect(&link, "localhost", argv[1], argv[2], argv[5], 0, NULL, 0);
+
+    if(!ret) {
+        fprintf(stderr, "ERROR: MySQL connect failed for the following reason: %s\n", mysql_error(&link));
+        fcloseall();
+        return 1;
+    }
+
+    if(mysql_select_db(&link, argv[5])) {
+        fprintf(stderr, "ERROR: Could not select MySQL database %s. MySQL said: %s", argv[5], mysql_error(&link));
+        fcloseall();
+        mysql_close(&link);
+        return 1;
+    }
+
+    if( VERBOSE )
+        fprintf( stdout, "Size of deepStarData structure: %d\n", sizeof( deepStarData ) );
+
+    /* Write file headers */
+    writeDataFileHeader(usfhead);
+    us_header_offset = ftell(usfhead);
+
+    /* Leave space for / write a deep magnitude limit specification in the data files */
+    maglim = (int)(-5.0 * 100);
+    fwrite(&maglim, 2, 1, usf); // Bogus entry
+
+    /* Write a HTM level specification in the data file */
+    htm_level = HTM_LEVEL;
+    fwrite(&htm_level, 1, 1, usf);
+
+    /* Leave space for a specification of MSpT (Maximum Stars per Trixel) in the data files */
+    MSpT_unnamed = 0;
+    fwrite(&MSpT_unnamed, 2, 1, usf); // Bogus entry
+
+
+    /* Initialize some variables */
+    lim = 0;
+    exitflag = 0;
+    current_trixel = 0;
+    usf_trix_count = 0;
+    usf_trix_begin = 2 + 1 + 2; // The 2 + 1 + 2 is to leave space for deep magnitude limit, HTM Level and MSpT specification. TODO: Change this if things change.
+    ntrixels = 0;
+
+    /* Recurse over every MYSQL_STARS_PER_QUERY DB entries */
+    while(!exitflag) {
+
+        /* Build MySQL query for next MYSQL_STARS_PER_QUERY stars */
+        sprintf( query, 
+                 "SELECT `Trixel`, `RA`, `Dec`, `dRA`, `dDec`, `B`, `V`, `UID` FROM `%s` ORDER BY `trixel`, `Mag` ASC LIMIT %ld, %d", 
+                 (argc >= 7) ? argv[6] : DB_TBL, lim, MYSQL_STARS_PER_QUERY );
+
+        if(VERBOSE) { fprintf(stderr, "SQL Query: %s\n", query); }
+    
+        /* Execute MySQL query and get the result */
+        mysql_real_query( &link, query, (unsigned int) strlen( query ) ); 
+        result = mysql_use_result( &link );
+      
+        if(!result) {
+            fprintf( stderr, "MySQL returned NULL result: %s\n", mysql_error( &link ) );
+            fcloseall();
+            mysql_close(&link);
+            return 1;
+        }
+      
+        exitflag = -1;
+      
+        /* Process the result row by row */
+        while(row = mysql_fetch_row(result)) {
+          
+            /* Very verbose details */
+            if(VERBOSE > 1) {
+                fprintf(stderr, "UID = %s\n", row[12]);
+                for(i = 0; i <= 14; ++i)
+                    fprintf(stderr, "\tField #%d = %s\n", i, row[i]);
+            }
+          
+            if(exitflag == -1)
+                exitflag = 0;
+          
+            /* Write index entries if we've changed trixel */
+            u_int32_t new_trixel = atoi( row[0] );
+            if( current_trixel != new_trixel ) { 
+                if(VERBOSE) { fprintf(stderr, "Trixel Changed from %d to %s = %d!\n", current_trixel, row[0], new_trixel ); }
+                while( new_trixel != current_trixel ) {
+                    writeIndexEntry(usfhead, current_trixel, us_header_offset + NTRIXELS * INDEX_ENTRY_SIZE + usf_trix_begin, usf_trix_count);
+                    usf_trix_begin = ftell( usf );
+                    if( usf_trix_count > MSpT_unnamed )
+                        MSpT_unnamed = usf_trix_count;
+                    current_trixel++;
+                    ntrixels++;
+                    usf_trix_count = 0;
+                    if( VERBOSE ) { fprintf( stderr, "%d %d\n", current_trixel - 1, ntrixels ); }
+                }
+                if( VERBOSE ) { fprintf( stderr, "Done writing trixel entry\n" ); }
+            }
+
+            /* Initializations */
+
+            usf_trix_count++;
+            f = usf;
+
+            /* Convert various fields and make entries into the deepStarData structure */
+            str2int32( &data.RA, row[1], 6 );
+            str2int32( &data.Dec, row[2], 5 );
+            str2int16( &data.dRA, row[3], 3 );
+            str2int16( &data.dDec, row[4], 3 );
+            str2int16( &data.B, row[5], 3 );
+            str2int16( &data.V, row[6], 3 );
+
+            int16_t B = data.B;
+            int16_t V = data.V;
+
+            if( V == 30000 ) {
+                if( B - 1600 > maglim && B != 30000 ) {
+                    maglim = B - 1600;
+                }
+            }
+            else {
+                if( V > maglim ) {
+                    maglim = V;
+                }
+            }
+
+            /* Write the data into the appropriate data file and any names into the name file */
+            if(VERBOSE) 
+                fprintf(stderr, "Writing UID = %s...", row[7]);
+            fwrite(&data, sizeof(deepStarData), 1, f);
+            if(VERBOSE) 
+                fprintf(stderr, "Done.\n");
+
+        }
+        mysql_free_result(result);
+        lim += MYSQL_STARS_PER_QUERY;
+    }
+
+    do {
+        writeIndexEntry(usfhead, current_trixel, us_header_offset + NTRIXELS * INDEX_ENTRY_SIZE + usf_trix_begin, usf_trix_count);
+        usf_trix_begin = ftell(usf);
+        usf_trix_count = 0;
+        current_trixel++;
+        ntrixels++;
+    }while( current_trixel != NTRIXELS );
+
+    if(ntrixels != NTRIXELS) {
+        fprintf(stderr, "ERROR: Expected %u trixels, but found %u instead. Please redefine NTRIXELS in this program, or check the source database for bogus trixels\n", NTRIXELS, ntrixels);
+    }
+
+    rewind(usf);
+    fwrite(&maglim, 2, 1, usf);
+    fwrite(&htm_level, 1, 1, usf);
+    fwrite(&MSpT_unnamed, 2, 1, usf);
+
+    fcloseall();
+    mysql_close(&link);
+    
+    return 0;
+}
