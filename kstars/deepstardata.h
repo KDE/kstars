0a5d49ad7a863b58854369cee039892e3f468fd8
Akarsh Simha <akarshsimha@gmail.com>
2008-08-13 15:10:47 +0000

    Adding structure to store and retrieve star data for very faint stars (which do not have information such as parallax, spectral type etc).
    CCMAIL: kstars-devel@kde.org
    
    
    svn path=/trunk/KDE/kdeedu/kstars/; revision=846485

---
+++ kstars/deepstardata.h
@@ -0,0 +1,76 @@
+/***************************************************************************
+                     stardata.h  -  K Desktop Planetarium
+                             -------------------
+    begin                : Tue 5 Aug 2008
+    copyright            : (C) 2008 by Akarsh Simha
+    email                : akarshsimha@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef DEEPSTARDATA_H
+#define DEEPSTARDATA_H
+
+#include <QtGlobal>
+
+/**
+ *@short  Structure that holds star data for really faint stars
+ *@author Akarsh Simha
+ *@version 1.0
+ */
+
+typedef struct deepStarData {
+    qint32 RA;
+    qint32 Dec;
+    qint16 dRA;
+    qint16 dDec;
+    qint16 B;
+    qint16 V;
+} deepStarData;
+
+#endif
+/***************************************************************************
+                     stardata.h  -  K Desktop Planetarium
+                             -------------------
+    begin                : Tue 5 Aug 2008
+    copyright            : (C) 2008 by Akarsh Simha
+    email                : akarshsimha@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef DEEPSTARDATA_H
+#define DEEPSTARDATA_H
+
+#include <QtGlobal>
+
+/**
+ *@short  Structure that holds star data for really faint stars
+ *@author Akarsh Simha
+ *@version 1.0
+ */
+
+typedef struct deepStarData {
+    qint32 RA;
+    qint32 Dec;
+    qint16 dRA;
+    qint16 dDec;
+    qint16 B;
+    qint16 V;
+} deepStarData;
+
+#endif
