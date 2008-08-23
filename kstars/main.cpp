c58b0403dd7fb115904e07c99231ea812f8f794d
Jason Harris <kstars@30doradus.org>
2008-08-23 14:17:07 +0000

    version bump
    svn path=/branches/KDE/4.1/kdeedu/kstars/; revision=851304

--- kstars/main.cpp
+++ kstars/main.cpp
@@ -32,7 +32,7 @@
 #include <QPixmap>
 #include <kglobal.h>
 
-#define KSTARS_VERSION "1.3.99"
+#define KSTARS_VERSION "1.4.1"
 
 static const char description[] =
     I18N_NOOP("Desktop Planetarium");
