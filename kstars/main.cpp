7199a759fed5f757ee358d22ba311efc500c172c
Prakash Mohan <prak902000@gmail.com>
2009-08-14 07:56:41 +0000

    Version bump after merging the summer branch.
    CCMAIL: kstars-devel@kde.org
    
    svn path=/trunk/KDE/kdeedu/kstars/; revision=1011265

--- kstars/main.cpp
+++ kstars/main.cpp
@@ -32,7 +32,7 @@
 #include <QPixmap>
 #include <kglobal.h>
 
-#define KSTARS_VERSION "1.5.4"
+#define KSTARS_VERSION "1.6.0"
 
 static const char description[] =
     I18N_NOOP("Desktop Planetarium");
