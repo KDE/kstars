8756152b6f7c75d502626fac7bdc9a4119b8b988
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2010-01-31 14:11:06 +0000

    Merge classes LineListComponent and SatelliteComponent. They were too closely related and SatelliteComponent was only subclass of LineListComponent
    Since satellites are disabled because of bugginess there are some
    more added
    
    svn path=/trunk/KDE/kdeedu/kstars/; revision=1082963

--- kstars/skycomponents/satellitecomponent.cpp
+++ kstars/skycomponents/satellitecomponent.cpp
@@ -23,10 +23,14 @@
 #include "kstarsdata.h"
 #include "skymap.h"
 #include "skyobjects/skyline.h"
+#include "skylabeler.h"
 #include "Options.h"
 
-SatelliteComponent::SatelliteComponent(SkyComponent *parent )
-        : LineListComponent(parent)
+SatelliteComponent::SatelliteComponent(SkyComponent *parent ) :
+    SkyComponent(parent),
+    LabelPosition( NoLabel ),
+    m_skyLabeler( SkyLabeler::Instance() )
+
 {}
 
 SatelliteComponent::~SatelliteComponent()
@@ -35,6 +39,19 @@ SatelliteComponent::~SatelliteComponent()
 bool SatelliteComponent::selected()
 {
     return Options::showSatellites();
+}
+
+// I don't think the ecliptic or the celestial equator should precess. -jbb
+void SatelliteComponent::update( KSNumbers * )
+{
+    if ( ! selected() )
+        return;
+    KStarsData *data = KStarsData::Instance();
+
+    foreach ( SkyPoint* p, pointList ) {
+        //if ( num ) p->updateCoords( num );
+        p->EquatorialToHorizontal( data->lst(), data->geo()->lat() );
+    }
 }
 
 void SatelliteComponent::init() {
@@ -47,11 +64,10 @@ void SatelliteComponent::init() {
 
 void SatelliteComponent::initSat( const QString &name, SPositionSat *pSat[], int npos ) {
     KStarsData *data = KStarsData::Instance();
-    setLabel( name );
-    setLabelPosition( LineListComponent::RightEdgeLabel );
-
-    setPen( QPen( QBrush( data->colorScheme()->colorNamed( "SatColor" ) ),
-                  2.5, Qt::SolidLine ) );
+    Label = name;
+    LabelPosition = RightEdgeLabel;
+    Pen = QPen( QBrush( data->colorScheme()->colorNamed( "SatColor" ) ),
+                2.5, Qt::SolidLine );
 
     for ( int i=0; i<npos; i++ ) {
         SkyPoint* p = new SkyPoint();
@@ -59,14 +75,111 @@ void SatelliteComponent::initSat( const QString &name, SPositionSat *pSat[], int
         p->setAz( pSat[i]->sat_azi );
         p->HorizontalToEquatorial( data->lst(), data->geo()->lat() );
 
-        appendP( p );
+        pointList.append( p );
         JDList.append( pSat[i]->jd );
 
     }
 }
 
+void SatelliteComponent::drawLinesComponent( QPainter &psky )
+{
+    if ( ! selected() )
+        return;
+
+    SkyMap *map = SkyMap::Instance();
+
+    psky.setPen( Pen );
+
+    bool isVisible, isVisibleLast;
+    SkyPoint  *pLast, *pThis;
+
+    // These are used to keep track of the element that is farrthest left,
+    // farthest right, etc.
+    float xLeft  = 100000.;
+    float xRight = 0.;
+    float yTop   = 100000.;
+    float yBot   = 0.;
+
+    // These are the indices of the farthest left point, farthest right point,
+    // etc.  The are data members so the drawLabels() routine can use them.
+    // Zero indicates an index that was never set and is considered invalid
+    // inside of drawLabels().
+    m_iLeft = m_iRight = m_iTop = m_iBot = 0;
+
+    // We don't draw the label here but we need the proper font in order to set
+    // the margins correctly.  Since psky no contains the zoom dependent font as
+    // its default font, we need to play the little dance below.
+    QFontMetricsF fm( psky.font() );
+
+    // Create the margins within which is is okay to draw the label
+    float sideMargin   =  fm.width("MM") + fm.width( Label ) / 2.0;
+
+    float leftMargin  = sideMargin;
+    float rightMargin = psky.window().width() - sideMargin;
+    float topMargin   = fm.height();
+    float botMargin   = psky.window().height() - 2.0 * fm.height();
+
+    QPointF oThis, oLast, oMid;
+
+    pLast = pointList.at( 0 );
+    oLast = map->toScreen( pLast, true, &isVisibleLast );
+
+    int limit = pointList.size();
+
+    for ( int i=1 ; i < limit ; i++ ) {
+        pThis = pointList.at( i );
+        oThis = map->toScreen( pThis, true, &isVisible );
+
+        if ( map->onScreen(oThis, oLast ) ) {
+            if ( isVisible && isVisibleLast ) {
+                psky.drawLine( oLast, oThis );
+
+                // Keep track of index of leftmost, rightmost, etc point.
+                // Only allow points that fit within the margins.
+                qreal x = oThis.x();
+                qreal y = oThis.y();
+                if ( x > leftMargin && x < rightMargin &&
+                        y < botMargin  && y > topMargin) {
+
+                    if ( x < xLeft ) {
+                        m_iLeft = i;
+                        xLeft = x;
+                    }
+                    if ( x > xRight ) {
+                        m_iRight = i;
+                        xRight = x;
+                    }
+                    if (  y > yBot ) {
+                        m_iBot = i;
+                        yBot = y;
+                    }
+                    if ( y < yTop ) {
+                        m_iTop = i;
+                        yTop = y;
+                    }
+                }
+            }
+
+            else if ( isVisibleLast && ! isVisible ) {
+                oMid = map->clipLine( pLast, pThis );
+                psky.drawLine( oLast, oMid );
+            }
+            else if ( isVisible && ! isVisibleLast ) {
+                oMid = map->clipLine( pThis, pLast );
+                psky.drawLine( oMid, oThis );
+            }
+        }
+
+        pLast = pThis;
+        oLast = oThis;
+        isVisibleLast = isVisible;
+    }
+
+    drawLabels( psky );
+}
+
 void SatelliteComponent::draw( QPainter &psky ) {
-    LineListComponent::draw( psky );
+    drawLinesComponent( psky );
 
     SkyMap *map = SkyMap::Instance();
     KStarsData *data = KStarsData::Instance();
@@ -81,12 +194,12 @@ void SatelliteComponent::draw( QPainter &psky ) {
     KStarsDateTime dtLast( JDList[0] );
     for ( int i=1; i<JDList.size(); ++i ) {
         KStarsDateTime dt( JDList[i] );
-        SkyPoint *sp = points()->at(i);
+        SkyPoint *sp = pointList.at(i);
         SkyPoint *sp2;
-        if ( i<points()->size()-1 )
-            sp2 = points()->at(i+1);
+        if ( i<pointList.size()-1 )
+            sp2 = pointList.at(i+1);
         else
-            sp2 = points()->at(i-1);
+            sp2 = pointList.at(i-1);
 
         if ( sp->alt()->Degrees() > 0.0
                 && dt.time().minute() != dtLast.time().minute() ) {
@@ -146,3 +259,101 @@ void SatelliteComponent::draw( QPainter &psky ) {
 
 }
 
+void SatelliteComponent::drawLabels( QPainter& psky )
+{
+    if ( LabelPosition == NoLabel ) return;
+
+    SkyMap *map = SkyMap::Instance();
+
+    double comfyAngle = 40.0;  // the first valid candidate with an angle
+    // smaller than this gets displayed.  If you set
+    // this to > 90. then the first valid candidate
+    // will be displayed, regardless of angle.
+
+    // We store info about the four candidate points in arrays to make several
+    // of the steps easier, particularly choosing the valid candiate with the
+    // smallest angle from the horizontal.
+
+    int     i[4];                                  // index of candidate
+    double  a[4] = { 360.0, 360.0, 360.0, 360.0 }; // angle, default to large value
+    QPointF o[4];                                  // candidate point
+    bool okay[4] = { true, true, true, true };     // flag  candidate false if it
+    // overlaps a previous label.
+
+    // Try candidate in different orders depending on if the label was to be
+    // near the left or right side of the screen.
+    if ( LabelPosition == LeftEdgeLabel ) {
+        i[0] = m_iLeft;
+        i[1] = m_iRight;
+        i[2] = m_iBot;
+        i[3] = m_iTop;
+    }
+    else {
+        i[0] = m_iRight;
+        i[1] = m_iLeft;
+        i[2] = m_iBot;
+        i[3] = m_iTop;
+    }
+
+    // Make sure we have at least one valid point
+    int firstI = 0;
+
+    for ( ; firstI < 4; firstI++ ) {
+        if ( i[firstI] ) break;
+    }
+
+    // return if there are no valid candidates
+    if ( firstI >= 4 ) return;
+
+
+    // Try the points in order and print the label if we can draw it at
+    // a comfortable angle for viewing;
+    for ( int j = firstI; j < 4; j++ ) {
+        o[j] = angleAt( map, i[j], &a[j] );
+        if ( fabs( a[j] ) > comfyAngle ) continue;
+        if ( m_skyLabeler->drawGuideLabel( psky, o[j], Label, a[j] ) )
+            return;
+        okay[j] = false;
+    }
+
+    //--- No angle was comfy so pick the one with the smallest angle ---
+
+    // Index of the index/angle/point that gets displayed
+    int bestI = firstI;
+
+    // find first valid candidate that does not overlap existing labels
+    for ( ; bestI < 4; bestI++ ) {
+        if ( i[bestI] && okay[bestI] ) break;
+    }
+
+    // return if all candiates either overlap or are invalid
+    if ( bestI >= 4 ) return;
+
+    // find the valid non-overlap candidate with the smallest angle
+    for ( int j = bestI + 1; j < 4; j++ ) {
+        if ( i[j] && okay[j] && fabs(a[j]) < fabs(a[bestI]) ) bestI = j;
+    }
+
+    m_skyLabeler->drawGuideLabel( psky, o[bestI], Label, a[bestI] );
+}
+
+
+QPointF SatelliteComponent::angleAt( SkyMap* map, int i, double *angle )
+{
+    SkyPoint* pThis = pointList.at( i );
+    SkyPoint* pLast = pointList.at( i - 1 );
+
+    QPointF oThis = map->toScreen( pThis );
+    QPointF oLast = map->toScreen( pLast );
+
+    double sx = double( oThis.x() - oLast.x() );
+    double sy = double( oThis.y() - oLast.y() );
+
+    *angle = atan2( sy, sx ) * 180.0 / dms::PI;
+
+    // Never draw the label upside down
+    if ( *angle < -90.0 ) *angle += 180.0;
+    if ( *angle >  90.0 ) *angle -= 180.0;
+
+    return oThis;
+}
--- kstars/skycomponents/satellitecomponent.h
+++ kstars/skycomponents/satellitecomponent.h
@@ -18,24 +18,49 @@
 #ifndef SATELLITECOMPONENT_H
 #define SATELLITECOMPONENT_H
 
+#include <QList>
+#include <QPointF>
+#include <QPen> 
 
-#include "linelistcomponent.h"
+#include "skycomponent.h"
+#include "skyobjects/skypoint.h"
+
 extern "C" {
 #include "satlib/SatLib.h"
 }
 
-class SatelliteComponent : public LineListComponent {
+class SkyMap;
+class SkyLabeler;
+
+
+class SatelliteComponent : public SkyComponent
+{
 public:
+    enum { NoLabel = 0, LeftEdgeLabel = 1, RightEdgeLabel = 2, UnknownLabel };
+
     /**@short Constructor
      * @p parent Pointer to the parent SkyComponent object
      */
     SatelliteComponent(SkyComponent* parent);
 
-    /**@short Destructor.  Delete list members */
-    ~SatelliteComponent();
+    virtual ~SatelliteComponent();
 
     /* Exists only to make this class constructible. */
     virtual void init();
+
+    /**@short Update the sky positions of this component.
+     *
+     * This function usually just updates the Horizontal (Azimuth/Altitude)
+     * coordinates of the objects in this component.  However, the precession
+     * and nutation must also be recomputed periodically.  Requests to do
+     * so are sent through the doPrecess parameter.
+     * @p num Pointer to the KSNumbers object
+     * @note By default, the num parameter is NULL, indicating that 
+     * Precession/Nutation computation should be skipped; this computation 
+     * is only occasionally required.
+     */
+    virtual void update( KSNumbers *num=0 );
+    
     virtual bool selected();
     virtual void draw( QPainter &psky );
 
@@ -43,6 +68,30 @@ public:
     void initSat(const QString &name, SPositionSat *pSat[], int nsteps);
 
 private:
+    void drawLinesComponent( QPainter &psky );
+    /* @short draw the label if any.  Is currently called at the bottom of
+     * draw() but that call could be removed and it could be called
+     * externally AFTER draw() has been called so draw() can set up the label
+     * position candidates.
+     */
+    void drawLabels( QPainter& psky );
+
+    /* @short This routine does two things at once.  It returns the QPointF
+     * corresponding to pointList[i] and also computes the angle using
+     * pointList[i] and pointList[i-1] therefore you MUST ensure that:
+     *
+     *       1 <= i < pointList.size().
+     */
+    QPointF angleAt( SkyMap* map, int i, double *angle );
+
+    QList<SkyPoint*> pointList;
+    int LabelPosition;
+    QString Label;
+    QPen Pen;
+
+    SkyLabeler* m_skyLabeler;
+    int m_iLeft, m_iRight, m_iTop, m_iBot; // the four label position candidates
+
     QStringList SatelliteNames;
     long double JulianDate;
     QList<double> JDList;
