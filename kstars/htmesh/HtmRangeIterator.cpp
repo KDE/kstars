d5be080d329713df7833da25ff5e1ea582f2335a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2010-02-23 11:54:04 +0000

    Fix indentation
    svn path=/trunk/KDE/kdeedu/kstars/; revision=1094954

--- kstars/htmesh/HtmRangeIterator.cpp
+++ kstars/htmesh/HtmRangeIterator.cpp
@@ -2,32 +2,32 @@
 
 Key HtmRangeIterator::next()
 {
-  Key key = this->nextval;
-  getNext();
-  return (Key) key;
+    Key key = this->nextval;
+    getNext();
+    return key;
 }
 
 void HtmRangeIterator::getNext()
 {
-  if (currange[0] <= 0){
-    nextval = -1;
-    return;
-  }
-  nextval++;
-  if (nextval > currange[1]){
-    range->getNext(&currange[0], &currange[1]);
     if (currange[0] <= 0){
-      nextval = -1;
-      return;
+        nextval = -1;
+        return;
     }
-    nextval = currange[0];
-  }
-  return;
+    nextval++;
+    if (nextval > currange[1]){
+        range->getNext(&currange[0], &currange[1]);
+        if (currange[0] <= 0){
+            nextval = -1;
+            return;
+        }
+        nextval = currange[0];
+    }
+    return;
 }
 
 bool HtmRangeIterator::hasNext()
 {
-  return (nextval > 0);
+    return (nextval > 0);
 }
 
 
--- kstars/htmesh/HtmRangeIterator.h
+++ kstars/htmesh/HtmRangeIterator.h
@@ -4,24 +4,24 @@
 #include <HtmRange.h>
 
 class HtmRangeIterator {
- public:
-  Key next();
-  bool hasNext();
-  HtmRangeIterator(HtmRange *ran) {
-    range = ran;
-    range->reset();
-    range->getNext(&currange[0], &currange[1]);
-    nextval = currange[0] - 1;
-    getNext();
-  }
- protected:
-  HtmRange *range;
-  void getNext();
+public:
+    Key next();
+    bool hasNext();
+    HtmRangeIterator(HtmRange *ran) {
+        range = ran;
+        range->reset();
+        range->getNext(&currange[0], &currange[1]);
+        nextval = currange[0] - 1;
+        getNext();
+    }
+protected:
+    HtmRange *range;
+    void getNext();
 
- private:
-  Key nextval;
-  Key currange[2];		/* Low and High */
-  HtmRangeIterator() : range(0), nextval(-1) {}
+private:
+    Key nextval;
+    Key currange[2];		/* Low and High */
+    HtmRangeIterator() : range(0), nextval(-1) {}
 
 };
 
