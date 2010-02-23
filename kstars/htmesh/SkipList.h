00128d7eb77cb586f891e1249cf2a79f16002e39
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2010-02-23 11:55:03 +0000

    Fix indentation
    svn path=/trunk/KDE/kdeedu/kstars/; revision=1094965

--- kstars/htmesh/SkipList.h
+++ kstars/htmesh/SkipList.h
@@ -18,62 +18,64 @@ class SkipListElement;
 
 class LINKAGE SkipList{
 public:
-  SkipList(float probability = 0.5);
-  ~SkipList();
+    SkipList(float probability = 0.5);
+    ~SkipList();
 
-  void insert(const Key searchKey, const Value value); // insert new element
-  Value searchAlt(const Key searchKey); // search element with key
-  Value search(const Key searchKey, const int iterator_flag); // search element with key
-  Value search(const Key searchKey); // search element with key
-  Key findMAX(const Key searchKey) const; // search element with key NGT searchKey
-  Key findMIN(const Key searchKey) const; // search element with key NLT searchKey
-  /* ITERATOR SUPPRT */
-  void reset() {iter = myHeader->getElement(0);}
-  int step() {
-    iter = iter->getElement(0); return (iter != NIL);}
-
-  Key getkey() {
-    if (iter != NIL)
-      return iter->getKey();
-    else
-      return (Key) -1;
-  }
-
-  Value getvalue() {
-    if (iter != NIL)
-      return iter->getValue();
-    else
-      return (Value) -1;
-  }
-
-  Key getNthKey(const int n){
-    int n_now = n-1;
-    iter = myHeader->getElement(0);
-    while(n_now > 0){
-      if (iter == NIL) 
-	break;
-      iter = iter->getElement(0);
-      n_now--;
+    void insert(const Key searchKey, const Value value); // insert new element
+    Value searchAlt(const Key searchKey); // search element with key
+    Value search(const Key searchKey, const int iterator_flag); // search element with key
+    Value search(const Key searchKey); // search element with key
+    Key findMAX(const Key searchKey) const; // search element with key NGT searchKey
+    Key findMIN(const Key searchKey) const; // search element with key NLT searchKey
+    /* ITERATOR SUPPRT */
+    void reset() {iter = myHeader->getElement(0);}
+    int step() {
+        iter = iter->getElement(0); return (iter != NIL);
     }
-    if (iter != NIL)
-      return iter->getKey();
 
-    return (Key) -1;
-  }
-  void free(const Key searchKey); // free element with key
-  void freeRange(const Key loKey, const Key hiKey);
-  void freeAll();
+    Key getkey() {
+        if (iter != NIL)
+            return iter->getKey();
+        else
+            return (Key) -1;
+    }
 
-  void stat(); // statistics;
-  long getLength() {
-    return myLength;
-  }
+    Value getvalue() {
+        if (iter != NIL)
+            return iter->getValue();
+        else
+            return (Value) -1;
+    }
+
+    Key getNthKey(const int n){
+        int n_now = n-1;
+        iter = myHeader->getElement(0);
+        while(n_now > 0){
+            if (iter == NIL) 
+                break;
+            iter = iter->getElement(0);
+            n_now--;
+        }
+        if (iter != NIL)
+            return iter->getKey();
+        else
+            return (Key) -1;
+    }
+
+    void free(const Key searchKey); // free element with key
+    void freeRange(const Key loKey, const Key hiKey);
+    void freeAll();
+
+    void stat(); // statistics;
+    long getLength() {
+        return myLength;
+    }
 
 private:
-  float myProbability;
-  SkipListElement* myHeader; // the header (first) list element
-  SkipListElement* iter;
-  long myLength;
+    float myProbability;
+    SkipListElement* myHeader; // the header (first) list element
+    SkipListElement* iter;
+    long myLength;
 };
 
 #endif // _SkipList_H
