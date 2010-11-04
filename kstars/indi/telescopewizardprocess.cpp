749d79f8f91f3ffcd7b7ca1a4861c359b8303854
Charles Sheridan <casheridan@gmail.com>
2010-11-04 02:28:53 +0000

    Adding a Cancel button to the Telescope Wizard dialog.
    CCBUG: 255847
    
    svn path=/trunk/KDE/kdeedu/kstars/; revision=1192806

--- kstars/indi/telescopewizardprocess.cpp
+++ kstars/indi/telescopewizardprocess.cpp
@@ -88,6 +88,7 @@ telescopeWizardProcess::telescopeWizardProcess( QWidget* parent, const char* /*n
     << "/dev/ttyUSB0" << "/dev/ttyUSB1" << "/dev/ttyUSB2" << "/dev/ttyUSB3";
 
     connect(ui->helpB, SIGNAL(clicked()), parent, SLOT(appHelpActivated()));
+    connect(ui->cancelB, SIGNAL(clicked()), this, SLOT(cancelCheck()));
     connect(ui->nextB, SIGNAL(clicked()), this, SLOT(processNext()));
     connect(ui->backB, SIGNAL(clicked()), this, SLOT(processBack()));
     connect(ui->setTimeB, SIGNAL(clicked()), this, SLOT(newTime()));
@@ -103,6 +104,25 @@ telescopeWizardProcess::~telescopeWizardProcess()
     Options::setShowINDIMessages( INDIMessageBar );
     delete ui;
     //Reset();
+}
+
+//Called when cancel is clicked, gives a warning if past the first couple of steps
+void telescopeWizardProcess::cancelCheck(void)
+{
+    int answer;
+    
+    switch (currentPage)
+    {
+    case TELESCOPE_P:
+    case LOCAL_P:
+    case PORT_P:
+	if (KMessageBox::warningYesNo(0, i18n("Are you sure you want to cancel?")) == 3)
+	    emit reject();
+	break;
+    default:
+	emit reject();
+	break;
+    }
 }
 
 void telescopeWizardProcess::processNext(void)
