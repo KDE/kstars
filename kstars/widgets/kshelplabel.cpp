24caa18434e5f9aacdb5030a7b3514be18651176
Victor Carbune <victor.carbune@gmail.com>
2010-12-01 13:42:41 +0000

    Added license information. Hope there isn't anything else I forgot.
    CCMAIL: kstars-devel@kde.org, kharvd@gmail.com
    
    svn path=/branches/kstars/code-in/kstars/; revision=1202590

--- kstars/widgets/kshelplabel.cpp
+++ kstars/widgets/kshelplabel.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+            kshelplabel.cpp - Help label used to document astronomical terms
+                             -------------------
+    begin                : Wed 1 Dec 2010
+    copyright            : (C) 2010 by Valery Kharitonov
+    email                : kharvd@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "kshelplabel.h"
 #include <ktoolinvocation.h>
 #include <QtGui/QMessageBox>
@@ -32,3 +49,4 @@ void KSHelpLabel::setText(const QString& txt) {
     m_cleanText = txt;
     QLabel::setText("<a href=\"ai-" + m_anchor + "\">" + m_cleanText + "</a>");
 }
+
--- kstars/widgets/kshelplabel.h
+++ kstars/widgets/kshelplabel.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+            kshelplabel.h - Help label used to document astronomical terms
+                             -------------------
+    begin                : Wed 1 Dec 2010
+    copyright            : (C) 2010 by Valery Kharitonov
+    email                : kharvd@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef KSHELPLABEL_H
 #define KSHELPLABEL_H
 
