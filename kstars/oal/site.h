2e68fa4b70f02628a9c330ea07ad0060942af500
Laurent Montel <montel@kde.org>
2011-02-06 16:22:20 +0000

    const'ify
    svn path=/trunk/KDE/kdeedu/kstars/; revision=1219157

--- kstars/oal/site.h
+++ kstars/oal/site.h
@@ -26,16 +26,16 @@
 
 class OAL::Site {
     public:
-       Site( QString id,  QString name, double lat, QString latUnit, double lon, QString lonUnit ) { setSite( id, name, lat, latUnit, lon, lonUnit ); }
-       Site( GeoLocation *geo, QString id ) { setSite( geo, id ); }
-       QString id() { return m_Id; }
-       QString name() { return m_Name; }
-       double latitude() { return m_Lat; }
-       QString latUnit() { return m_LatUnit; }
-       double longitude() { return m_Lon; }
-       QString lonUnit() { return m_LonUnit; }
-       void setSite( QString _id, QString _name, double _lat, QString _latUnit, double _lon, QString _lonUnit);
-       void setSite( GeoLocation *geo, QString id );
+       Site( const QString& id,  const QString& name, double lat, const QString& latUnit, double lon, const QString &lonUnit ) { setSite( id, name, lat, latUnit, lon, lonUnit ); }
+       Site( GeoLocation *geo, const QString& id ) { setSite( geo, id ); }
+       QString id() const { return m_Id; }
+       QString name() const { return m_Name; }
+       double latitude() const { return m_Lat; }
+       QString latUnit() const { return m_LatUnit; }
+       double longitude() const { return m_Lon; }
+       QString lonUnit() const { return m_LonUnit; }
+       void setSite( const QString &_id, const QString& _name, double _lat, const QString& _latUnit, double _lon, const QString& _lonUnit);
+       void setSite( GeoLocation *geo, const QString& id );
     private:
         QString m_Name, m_LatUnit, m_LonUnit, m_Id;
         double m_Lat, m_Lon;
