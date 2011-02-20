1bba20322fe03036f44ec9dccd1a5b0e55bd072e
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2011-02-20 18:03:44 +0000

    Remove unused function and reformat comment
    svn path=/trunk/KDE/kdeedu/kstars/; revision=1221852

--- kstars/skyobjects/ksmoon.h
+++ kstars/skyobjects/ksmoon.h
@@ -61,37 +61,28 @@ public:
     	*/
     QString phaseName( void ) const;
 
-    /** reimplemented from KSPlanetBase
-    	*/
+    /** reimplemented from KSPlanetBase */
     virtual bool loadData();
 
-    /**
-     * @return iPhase, which is used as a key to find the right image file
-     */
+    /** @return iPhase, which is used as a key to find the right image file */
     inline short int getIPhase() { return iPhase; }
 
-    /**
-     * @return the file name for the correct moon image
+    /** Reimplemented from KSPlanetBase, this function employs unique algorithms for
+     *  estimating the lunar coordinates.  Finding the position of the moon is
+     * much more difficult than the other planets.  For one thing, the Moon is
+     * a lot closer, so we can detect smaller deviations in its orbit.  Also,
+     * the Earth has a significant effect on the Moon's orbit, and their
+     * interaction is complex and nonlinear.  As a result, the positions as
+     * calculated by findPosition() are only accurate to about 10 arcseconds
+     * (10 times less precise than the planets' positions!)
+     * @short moon-specific coordinate finder
+     * @param num KSNumbers pointer for the target date/time
+     * @note we don't use the Earth pointer here
      */
-    inline QString getImageFileName() { return imFileName; }
-
-    /**Reimplemented from KSPlanetBase, this function employs unique algorithms for
-    	*estimating the lunar coordinates.  Finding the position of the moon is
-    	*much more difficult than the other planets.  For one thing, the Moon is
-    	*a lot closer, so we can detect smaller deviations in its orbit.  Also,
-    	*the Earth has a significant effect on the Moon's orbit, and their
-    	*interaction is complex and nonlinear.  As a result, the positions as
-    	*calculated by findPosition() are only accurate to about 10 arcseconds
-    	*(10 times less precise than the planets' positions!)
-    	*@short moon-specific coordinate finder
-    	*@param num KSNumbers pointer for the target date/time
-    	*@note we don't use the Earth pointer here
-    	*/
     virtual bool findGeocentricPosition( const KSNumbers *num, const KSPlanetBase* );
 
 private:
     virtual void initPopupMenu( KSPopupMenu* pmenu );
-
     virtual void findMagnitude(const KSNumbers*);
 
     static bool data_loaded;
@@ -131,7 +122,6 @@ private:
     };
 
     static QList<MoonBData> BData;
-    QString imFileName;
     unsigned int iPhase;
 };
 
