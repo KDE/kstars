788717fefbf63b0c05fcda37cf7c8eca7123300c
Rafał Kułaga <rl.kulaga@gmail.com>
2011-07-10 20:24:07 +0200

    Abort image export when save URL dialog is cancelled.

--- kstars/kstarsactions.cpp
+++ kstars/kstarsactions.cpp
@@ -519,6 +519,11 @@ void KStars::slotOpenFITS()
 void KStars::slotExportImage() {
     KUrl fileURL = KFileDialog::getSaveUrl( QDir::homePath(), "image/png image/jpeg image/gif image/x-portable-pixmap image/bmp image/svg+xml" );
 
+    //User cancelled file selection dialog - abort image export
+    if ( fileURL.isEmpty() ) {
+        return;
+    }
+
     //Warn user if file exists!
     if (QFile::exists(fileURL.path()))
     {
