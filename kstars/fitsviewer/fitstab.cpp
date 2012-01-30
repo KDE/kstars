dd96472d6c1bf8e645a0245521756fb7945049bf
Jasem Mutlaq <mutlaqja@ikarustech.com>
2012-01-30 04:26:18 +0300

    Adding license to files.

--- kstars/fitsviewer/fitstab.cpp
+++ kstars/fitsviewer/fitstab.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+                          FITS Tab
+                             -------------------
+    copyright            : (C) 2012 by Jasem Mutlaq
+    email                : mutlaqja@ikarustech.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <QClipboard>
 
 #include <KUndoStack>
