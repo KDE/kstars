87f9545c10563129e14686ff3141ae4c80679d33
Samikshan Bairagya <samikshan@gmail.com>
2012-03-09 07:27:38 +0530

    Add author and license information for the following files: kstars/skycomponents/notifyupdatesui.h kstars/skycomponents/notifyupdatesui.cpp

--- kstars/skycomponents/notifyupdatesui.cpp
+++ kstars/skycomponents/notifyupdatesui.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+                          notifyupdatesui.cpp  -  K Desktop Planetarium
+                             -------------------
+    begin                : 2012/05/03
+    copyright            : (C) 2012 by Samikshan Bairagya
+    email                : samikshan@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #include "skyobjects/supernova.h"
 #include "supernovaecomponent.h"
 #include "notifyupdatesui.h"
--- kstars/skycomponents/notifyupdatesui.h
+++ kstars/skycomponents/notifyupdatesui.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+                          notifyupdatesui.h  -  K Desktop Planetarium
+                             -------------------
+    begin                : 2012/05/03
+    copyright            : (C) 2012 by Samikshan Bairagya
+    email                : samikshan@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #ifndef NOTIFYUPDATESUI_H
 #define NOTIFYUPDATESUI_H
 
