c8dd5af7bff7f738ce2b6ab68bdb46e9125bde7d
Rafal Kulaga <rl.kulaga@gmail.com>
2012-07-16 00:13:23 +0200

    Deleting broken AAVSO feature.
    This feature had been broken for quite some time, so we've decided to
    remove it. Due to the big changes at AAVSO site, it might be quite hard
    to get it running in the future. Fortunately, we have one person who is
    working on it right now.
    
    CCMAIL: kstars-devel@kde.org

--- kstars/kstars.cpp
+++ kstars/kstars.cpp
@@ -51,15 +51,14 @@
 
 KStars *KStars::pinstance = 0;
 
-KStars::KStars( bool doSplash, bool clockrun, const QString &startdate ) :
-        KXmlGuiWindow(), kstarsData(0), skymap(0), TimeStep(0),
-        colorActionMenu(0), fovActionMenu(0),
-        AAVSODialog(0), findDialog(0), imgExportDialog(0), obsList(0),
-        execute(0),
-        avt(0), wut(0), skycal(0),
-    sb(0), pv(0), jmt(0), fm(0), astrocalc(0), printingWizard(0), indimenu(0), indidriver(0),
-        indiseq(0), ekosmenu(0), DialogIsObsolete(false), StartClockRunning( clockrun ),
-        StartDateString( startdate )
+KStars::KStars( bool doSplash, bool clockrun, const QString &startdate )
+    : KXmlGuiWindow(), kstarsData(0), skymap(0), TimeStep(0),
+      colorActionMenu(0), fovActionMenu(0), findDialog(0),
+      imgExportDialog(0), obsList(0), execute(0),
+      avt(0), wut(0), skycal(0), sb(0), pv(0), jmt(0),
+      fm(0), astrocalc(0), printingWizard(0), indimenu(0), indidriver(0),
+      indiseq(0), ekosmenu(0), DialogIsObsolete(false), StartClockRunning( clockrun ),
+      StartDateString( startdate )
 {
     new KstarsAdaptor(this);
     QDBusConnection::sessionBus().registerObject("/KStars",  this);
