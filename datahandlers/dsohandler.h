b16381367cb9be1255c93bec0ffc972c00302665
Rishab Arora <ra.rishab@gmail.com>
2012-08-05 01:38:56 +0530

    Added information on DB structure

--- datahandlers/dsohandler.h
+++ datahandlers/dsohandler.h
@@ -18,6 +18,13 @@
 #ifndef DSOHANDLER_H
 #define DSOHANDLER_H
 
+/* Some notes about the database. (skycomponents.db)
+ * 1) The uid for Object Designation is the uid being used by objects in KStars
+ *    hence, the uid is a qint64 i.e. a 64 bit signed integer. Coincidentaly,
+ *    this is the max limit of an int in Sqlite3.
+ *    
+ */
+
 class DSOHandler
 {
 };
