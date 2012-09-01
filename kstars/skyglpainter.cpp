f53bbe3622a7618b193fef2911657ff37ef140dc
Akarsh Simha <akarsh.simha@kdemail.net>
2012-09-01 04:20:14 -0500

    Fix the bug which painted stars of unknown spectral types in black.
    BUG: 306122

--- kstars/skyglpainter.cpp
+++ kstars/skyglpainter.cpp
@@ -163,6 +163,7 @@ bool SkyGLPainter::addItem(SkyPoint* p, int type, float width, char sp)
         case 'k': case 'K': starColor.setRgb( 255, 193, 153); break;
         case 'm': case 'M': starColor.setRgb( 255, 153, 153); break;
         case 'x':           starColor.setRgb( m_pen[0] * 255, m_pen[1] * 255, m_pen[2] *255 ); break;
+        default:            starColor.setRgb( 153, 255, 255); break; // If we don't know what spectral type, we use the same as 'A' (See SkyQPainter)
         }
 
         // Convert to HSV space using QColor's methods and adjust saturation.
