35b892697b1dd77c9ca6a70f7e7be9964f895032
Jasem Mutlaq <mutlaqja@ikarustech.com>
2012-10-20 18:40:22 +0300

    Filters start from 1

--- kstars/indi/opsindi.cpp
+++ kstars/indi/opsindi.cpp
@@ -40,7 +40,7 @@ OpsINDI::OpsINDI( KStars *_ks )
         kcfg_fitsDir->setText ( Options::fitsDir());
 
     if (Options::filterAlias().empty())
-        m_filterList << "0" << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9";
+        m_filterList << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "10";
     else
         m_filterList = Options::filterAlias();
 
