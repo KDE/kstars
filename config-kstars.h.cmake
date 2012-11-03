8b6deb1c3627a2c152f98ff8aac450fe840bdea9
Pino Toscano <pino@kde.org>
2012-11-03 16:24:50 +0100

    remove check for linux/videodev2.h (unused)

--- config-kstars.h.cmake
+++ config-kstars.h.cmake
@@ -1,6 +1,3 @@
-/* Define to 1 if you have the <linux/videodev2.h> header file. */
-#cmakedefine HAVE_LINUX_VIDEODEV2_H 1
-
 /* The symbol timezone is an int, not a function */
 #define TIMEZONE_IS_INT 1
 
