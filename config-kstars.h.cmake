1ef921bec4fbb218ef459839dd90c9e98097976b
Pino Toscano <pino@kde.org>
2012-11-03 16:26:15 +0100

    remove check for termios.h (unused)

--- config-kstars.h.cmake
+++ config-kstars.h.cmake
@@ -1,9 +1,6 @@
 /* The symbol timezone is an int, not a function */
 #define TIMEZONE_IS_INT 1
 
-/* Define if you have termios.h */
-#cmakedefine   HAVE_TERMIOS_H 1
-
 /* Define if you have fitsio.h */
 #cmakedefine   HAVE_CFITSIO_H 1
 
