29aadb50e0918085c3ef659594ee067c2bcc7571
Jasem Mutlaq <mutlaqja@ikarustech.com>
2014-07-02 01:08:43 +0300

    Check if coords are valid for telescope crosshair

--- kstars/skymapdrawabstract.cpp
+++ kstars/skymapdrawabstract.cpp
@@ -246,6 +246,9 @@ void SkyMapDrawAbstract::drawTelescopeSymbols(QPainter &psky)
             indi_sp.EquatorialToHorizontal( m_KStarsData->lst(), m_KStarsData->geo()->lat() );
 
 
+        if (std::isnan(indi_sp.ra().Degrees() || std::isnan(indi_sp.dec().Degrees())))
+            continue;
+
         QPointF P = m_SkyMap->m_proj->toScreen( &indi_sp );
         if ( Options::useAntialias() )
         {
