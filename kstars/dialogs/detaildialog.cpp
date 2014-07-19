18350f66f20de7a7f9b3d2e93c0eb23ffab8e784
Jasem Mutlaq <mutlaqja@ikarustech.com>
2014-07-19 23:34:55 +0300

    fix airmass precision

--- kstars/dialogs/detaildialog.cpp
+++ kstars/dialogs/detaildialog.cpp
@@ -495,7 +495,7 @@ void DetailDialog::createPositionTab( const KStarsDateTime &ut, GeoLocation *geo
     //equivalent to 1./sin(Alt).  Beware of Inf at Alt=0!
     if ( selectedObject->alt().Degrees() > 0.0 )
         Pos->Airmass->setText( QLocale().toString(
-                                   1./sin( selectedObject->alt().radians() ), 2 ) );
+                                   1./sin( selectedObject->alt().radians() ), 'f', 2 ) );
     else
         Pos->Airmass->setText( "--" );
 
