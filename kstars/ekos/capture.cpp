c345309a43a2d5f246eb6257eeda35948560feab
Jasem Mutlaq <mutlaqja@ikarustech.com>
2014-07-28 02:53:09 +0300

    send filter change in every capture command to be sure. Check dithering if autoguiding is enabled

--- kstars/ekos/capture.cpp
+++ kstars/ekos/capture.cpp
@@ -100,6 +100,10 @@ void SequenceJob::prepareCapture()
 
 SequenceJob::CAPTUREResult SequenceJob::capture(bool isDark)
 {
+
+    if (filterPos != -1 && activeFilter != NULL)
+        activeFilter->runCommand(INDI_SET_FILTER, &filterPos);
+
    if (activeChip->canSubframe() && activeChip->setFrame(x, y, w, h) == false)
    {
         status = JOB_ERROR;
@@ -390,6 +394,7 @@ void Capture::stopSequence()
         activeJob->reset();
     }
 
+    secondsLabel->clear();
     currentCCD->disconnect(this);
 
     currentCCD->setFITSDir("");
@@ -706,18 +711,13 @@ void Capture::newFITS(IBLOB *bp)
     if (isAutoFocus)
         autoFocusStatus = false;
 
-    if (isAutoGuiding)
-    {
-        if (currentCCD->getChip(ISD::CCDChip::GUIDE_CCD) == guideChip)
-            emit suspendGuiding(false);
+    if (isAutoGuiding && currentCCD->getChip(ISD::CCDChip::GUIDE_CCD) == guideChip)
+        emit suspendGuiding(false);
 
-        if (guideDither)
-        {
+    if (isAutoGuiding && guideDither)
+    {
             secondsLabel->setText(i18n("Dithering..."));
             emit exposureComplete();
-        }
-        else
-            seqTimer->start(seqDelay);
     }
     else if (isAutoFocus)
     {
