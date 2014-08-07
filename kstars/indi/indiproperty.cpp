cd35f9cbda54306fb904ddc7eb4010fdd0c83f2d
Jasem Mutlaq <mutlaqja@ikarustech.com>
2014-08-07 12:29:12 +0300

    Save state before reset for ATMOST1

--- kstars/indi/indiproperty.cpp
+++ kstars/indi/indiproperty.cpp
@@ -420,7 +420,11 @@ void INDI_P::newSwitch(const QString & name)
     else
     {
         if (svp->r == ISR_ATMOST1)
+        {
+            ISState prev_state = sp->s;
             IUResetSwitch(svp);
+            sp->s = prev_state;
+        }
 
         sp->s = (sp->s == ISS_ON) ? ISS_OFF : ISS_ON;
     }
