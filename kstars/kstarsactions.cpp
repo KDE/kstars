0e43c332750690dc77cff23b4ec7d20c33cd6295
Marcos CARDINOT <mcardinot@gmail.com>
2015-07-01 10:23:00 -0300

    forcing repaint after opening the SkyGuide window

--- kstars/kstarsactions.cpp
+++ kstars/kstarsactions.cpp
@@ -341,6 +341,8 @@ void KStars::slotSkyGuide() {
     // toggle visibility
     bool isVisible = m_SkyGuideMgr->view()->dock()->isVisible();
     m_SkyGuideMgr->view()->dock()->setVisible(!isVisible);
+    this->update();
+    this->repaint();
 }
 
 //FIXME Port to QML2
