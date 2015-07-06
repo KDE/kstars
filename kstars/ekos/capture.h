fa915c99db6dabf8d06e4567b338e9f4eacf65e4
Jasem Mutlaq <mutlaqja@ikarustech.com>
2015-07-06 22:15:29 +0300

    Update DBus documentation

--- kstars/ekos/capture.h
+++ kstars/ekos/capture.h
@@ -43,13 +43,18 @@ class KDirWatch;
  * - Auto guiding with support for automatic dithering between exposures and support for Adaptive Optics devices in addition to traditional guiders.
  * - Powerful sequence queue for batch capture of images with optional prefixes, timestamps, filter wheel selection, and much more!
  * - Export and import sequence queue sets as Ekos Sequence Queue (.esq) files.
+ * - Center the telescope anywhere in a captured FITS image or any FITS with World Coordinate System (WCS) header.
+ * - Automatic flat field capture, just set the desired ADU and let Ekos does the rest!
  * - Automatic abort and resumption of exposure tasks if guiding errors exceed a user-configurable value.
  * - Support for dome slaving.
  * - Complete integration with KStars Observation Planner and SkyMap
  * - Integrate with all INDI native devices.
  * - Powerful scripting capabilities via \ref EkosDBusInterface "DBus."
+ *
+ * The primary class is EkosManager. It handles startup and shutdown of local and remote INDI devices, manages and orchesterates the various Ekos modules, and provides advanced DBus
+ * interface to enable unattended scripting.
 *@author Jasem Mutlaq
- *@version 1.0
+ *@version 1.1
  */
 namespace Ekos
 {
