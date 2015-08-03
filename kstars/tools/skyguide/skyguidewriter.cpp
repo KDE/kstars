1dcd8ed014056ae2fef204f6ca1602eb53725b46
Marcos CARDINOT <mcardinot@gmail.com>
2015-08-03 19:00:00 +0100

    adding icons to the push buttons

--- kstars/tools/skyguide/skyguidewriter.cpp
+++ kstars/tools/skyguide/skyguidewriter.cpp
@@ -23,11 +23,25 @@ SkyGuideWriterUI::SkyGuideWriterUI(QWidget *parent) : QFrame(parent) {
 
 SkyGuideWriter::SkyGuideWriter(QWidget *parent) : QDialog(parent),
                                                   m_ui(new SkyGuideWriterUI) {
+    // setup main frame
     QVBoxLayout *mainLayout = new QVBoxLayout();
     mainLayout->addWidget(m_ui);
     setLayout(mainLayout);
     setWindowTitle(xi18n("SkyGuide Writer"));
     setModal( false );
+
+    // add icons to push buttons
+    m_ui->bNew->setIcon(QIcon::fromTheme("document-new"));
+    m_ui->bOpen->setIcon(QIcon::fromTheme("document-open"));
+    m_ui->bSave->setIcon(QIcon::fromTheme("document-save"));
+    m_ui->bSaveAs->setIcon(QIcon::fromTheme("document-save-as"));
+    m_ui->bInstall->setIcon(QIcon::fromTheme("system-software-install"));
+    m_ui->bAddAuthor->setIcon(QIcon::fromTheme("list-add"));
+    m_ui->bEditAuthor->setIcon(QIcon::fromTheme("insert-text"));
+    m_ui->bRemoveAuthor->setIcon(QIcon::fromTheme("list-remove"));
+    m_ui->bAddSlide->setIcon(QIcon::fromTheme("list-add"));
+    m_ui->bEditSlide->setIcon(QIcon::fromTheme("insert-text"));
+    m_ui->bRemoveSlide->setIcon(QIcon::fromTheme("list-remove"));
 }
 
 SkyGuideWriter::~SkyGuideWriter() {
