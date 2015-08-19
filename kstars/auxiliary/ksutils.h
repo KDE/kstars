0b8c6c32006d54ca999368b2f4df11c88fb5c3bf
Jasem Mutlaq <mutlaqja@ikarustech.com>
2015-08-19 13:48:43 +0300

    Add logging support to KStars

--- kstars/auxiliary/ksutils.h
+++ kstars/auxiliary/ksutils.h
@@ -137,6 +137,53 @@ namespace KSUtils {
     QList<SkyObject *> * castStarObjListToSkyObjList( QList<StarObject *> *starObjList );
 
 
+       /**
+       * Interface into Qt's logging system
+       * @Author: Yale Dedis 2011
+       * Adapted from DeDiS project.
+       */
+      class Logging {
+        public:
+          /**
+           * Store all logs into the specified file
+           * @param filename the file in which to store logs
+           */
+          static void UseFile(const QString &filename);
+
+          /**
+           * Output logs to stdout
+           */
+          static void UseStdout();
+
+          /**
+           * Output logs to stderr
+           */
+          static void UseStderr();
+
+          /**
+           * Use the default logging mechanism
+           */
+          static void UseDefault();
+
+          /**
+           * Disable logging
+           */
+          static void Disable();
+
+        private:
+          static QString _filename;
+
+          static void Disabled(QtMsgType type, const QMessageLogContext &context,
+              const QString &msg);
+          static void File(QtMsgType type, const QMessageLogContext &context,
+              const QString &msg);
+          static void Stdout(QtMsgType type, const QMessageLogContext &context,
+              const QString &msg);
+          static void Stderr(QtMsgType type, const QMessageLogContext &context,
+              const QString &msg);
+          static void Write(QTextStream &stream, QtMsgType type, const QString &msg);
+
+      };
 }
 
 #endif
