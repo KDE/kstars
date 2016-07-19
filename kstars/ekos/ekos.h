635296e1239b7a55b8beaadd523f8a5157e8e41d
Jasem Mutlaq <mutlaqja@ikarustech.com>
2016-07-19 23:50:52 +0300

    Add FocusState

--- kstars/ekos/ekos.h
+++ kstars/ekos/ekos.h
@@ -5,6 +5,8 @@
 
 namespace Ekos
 {
+
+    // Guide States
     static const QStringList guideStates = { I18N_NOOP("Idle"), I18N_NOOP("Calibrating"), I18N_NOOP("Calibration successful"), I18N_NOOP("Calibration error"), I18N_NOOP("Guiding"),
                                              I18N_NOOP("Aborted"), I18N_NOOP("Suspended"), I18N_NOOP("Dithering"), I18N_NOOP("Dithering Successful"), I18N_NOOP("Dithering error")};
 
@@ -13,6 +15,7 @@ namespace Ekos
 
     const QString & getGuideStatusString(GuideState state);
 
+    // Capture States
     static const QStringList captureStates = { I18N_NOOP("Idle"), I18N_NOOP("In Progress"), I18N_NOOP("Capturing"), I18N_NOOP("Aborted"), I18N_NOOP("Waiting"), I18N_NOOP("Image Received"),
                                                I18N_NOOP("Dithering"), I18N_NOOP("Focusing"), I18N_NOOP("Changing Filter"), I18N_NOOP("Setting Temperature"), I18N_NOOP("Aligning"),
                                                I18N_NOOP("Calibrating"), I18N_NOOP("Meridian Flip"), I18N_NOOP("Complete") };
@@ -22,6 +25,14 @@ namespace Ekos
 
 
     const QString &getCaptureStatusString(CaptureState state);
+
+    // Focus States
+    static const QStringList focusStates = { I18N_NOOP("Idle"), I18N_NOOP("Complete"), I18N_NOOP("Failed"), I18N_NOOP("Aborted"), I18N_NOOP("In Progress"),
+                                             I18N_NOOP("Capturing"), I18N_NOOP("Framing"), I18N_NOOP("User Input"), I18N_NOOP("Changing Filter")};
+
+    typedef enum { FOCUS_IDLE, FOCUS_COMPLETE, FOCUS_FAILED, FOCUS_ABORTED, FOCUS_PROGRESS, FOCUS_FRAMING, FOCUS_WAITING, FOCUS_CHANGING_FILTER} FocusState;
+
+    const QString &getFocusStatusString(FocusState state);
 
 }
 
