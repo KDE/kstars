KDE/kstars

commit 40cf6ad6cd9a8773b78da828daf98069f4777d76

Initial work for Ekos support under Windows

cmake/modules/FindINDI.cmake
@@ -3,71 +3,100 @@
 #
 #  INDI_FOUND - system has INDI
 #  INDI_INCLUDE_DIR - the INDI include directory
-#  INDI_LIBRARIES - Link these to use INDI
-#  INDI_DRIVER_LIBRARIES - Link to these to build INDI drivers with indibase support
+#  INDI_LIBRARIES - Link to these for XML and INDI Common support
 #  INDI_CLIENT_LIBRARIES - Link to these to build INDI clients
-#  INDI_DATA_DIR - INDI shared data dir.
 
-# Copyright (c) 2015, Jasem Mutlaq <mutlaqja@ikarustech.com>
+# Copyright (c) 2016, Jasem Mutlaq <mutlaqja@ikarustech.com>
 # Copyright (c) 2012, Pino Toscano <pino@kde.org>
 # Based on FindLibfacile by Carsten Niehaus, <cniehaus@gmx.de>
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
+# Redistribution ANDuse is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if (INDI_INCLUDE_DIR AND INDI_DATA_DIR AND INDI_LIBRARIES AND INDI_DRIVER_LIBRARIES AND INDI_CLIENT_LIBRARIES)
+macro(_INDI_check_version)
+  file(READ "${INDI_INCLUDE_DIR}/indiapi.h" _INDI_version_header)
+
+  string(REGEX MATCH "#define INDI_VERSION_MAJOR[ \t]+([0-9]+)" _INDI_version_major_match "${_INDI_version_header}")
+  set(INDI_VERSION_MAJOR "${CMAKE_MATCH_1}")
+  string(REGEX MATCH "#define INDI_VERSION_MINOR[ \t]+([0-9]+)" _INDI_version_minor_match "${_INDI_version_header}")
+  set(INDI_VERSION_MINOR "${CMAKE_MATCH_1}")
+  string(REGEX MATCH "#define INDI_VERSION_RELEASE[ \t]+([0-9]+)" _INDI_version_release_match "${_INDI_version_header}")
+  set(INDI_VERSION_RELEASE "${CMAKE_MATCH_1}")
+
+  set(INDI_VERSION ${INDI_VERSION_MAJOR}.${INDI_VERSION_MINOR}.${INDI_VERSION_RELEASE})
+  if(${INDI_VERSION} VERSION_LESS ${INDI_FIND_VERSION})
+    set(INDI_VERSION_OK FALSE)
+  else(${INDI_VERSION} VERSION_LESS ${INDI_FIND_VERSION})
+    set(INDI_VERSION_OK TRUE)
+  endif(${INDI_VERSION} VERSION_LESS ${INDI_FIND_VERSION})
+
+  if(NOT INDI_VERSION_OK)
+    message(STATUS "INDI version ${INDI_VERSION} found in ${INDI_INCLUDE_DIR}, "
+                   "but at least version ${INDI_FIND_VERSION} is required")
+  else(NOT INDI_VERSION_OK)
+      mark_as_advanced(INDI_VERSION_MAJOR INDI_VERSION_MINOR INDI_VERSION_RELEASE)
+  endif(NOT INDI_VERSION_OK)
+endmacro(_INDI_check_version)
+
+if (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND INDI_CLIENT_LIBRARIES)
 
   # in cache already
-  set(INDI_FOUND TRUE)
-  message(STATUS "Found INDI: ${INDI_LIBRARIES}")
+  _INDI_check_version()
+  set(INDI_FOUND ${INDI_VERSION_OK})
+  message(STATUS "Found INDI: ${INDI_LIBRARIES}, ${INDI_CLIENT_LIBRARIES}, ${INDI_INCLUDE_DIR}")
 
+else (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND INDI_CLIENT_LIBRARIES)
 
-else (INDI_INCLUDE_DIR AND INDI_DATA_DIR AND INDI_LIBRARIES AND INDI_DRIVER_LIBRARIES AND INDI_CLIENT_LIBRARIES)
+  if (NOT WIN32)
+    find_package(PkgConfig)
+    if (PKG_CONFIG_FOUND)
+      pkg_check_modules(PC_INDI INDI)
+    endif (PKG_CONFIG_FOUND)
+  endif (NOT WIN32)
 
-  find_package(PkgConfig)
-
-  if (PKG_CONFIG_FOUND)
-    if (INDI_FIND_VERSION)
-      set(version_string ">=${INDI_FIND_VERSION}")
-    endif()
-    pkg_check_modules(PC_INDI libindi${version_string})
-  else()
-    # assume it was found
-    set(PC_INDI_FOUND TRUE)
-  endif()
-
-  if (PC_INDI_FOUND)
-    find_path(INDI_INCLUDE_DIR indidevapi.h
-      PATH_SUFFIXES libindi
-      HINTS ${PC_INDI_INCLUDE_DIRS}
-    )
-
-    find_library(INDI_LIBRARIES NAMES indi
-      HINTS ${PC_INDI_LIBRARY_DIRS}
-    )
-
-    find_library(INDI_DRIVER_LIBRARIES NAMES indidriver
-      HINTS ${PC_INDI_LIBRARY_DIRS}
-    )
-    
-    find_library(INDI_CLIENT_LIBRARIES NAMES indiclient
-      HINTS ${PC_INDI_LIBRARY_DIRS}
-    )
-
-    find_path(INDI_DATA_DIR drivers.xml
-      PATH_SUFFIXES share/indi
-    )
-
-    set(INDI_VERSION "${PC_INDI_VERSION}")
-
-  endif()
-
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(INDI
-                                    REQUIRED_VARS INDI_INCLUDE_DIR INDI_LIBRARIES INDI_DRIVER_LIBRARIES INDI_CLIENT_LIBRARIES
-                                    VERSION_VAR INDI_VERSION
+  find_path(INDI_INCLUDE_DIR indidevapi.h
+    PATH_SUFFIXES libindi
+    ${PC_INDI_INCLUDE_DIRS}
+    ${_obIncDir}
+    ${GNUWIN32_DIR}/include
   )
 
-  mark_as_advanced(INDI_INCLUDE_DIR INDI_DATA_DIR INDI_LIBRARIES INDI_DRIVER_LIBRARIES INDI_CLIENT_LIBRARIES)
+  _INDI_check_version()
 
-endif (INDI_INCLUDE_DIR AND INDI_DATA_DIR AND INDI_LIBRARIES AND INDI_DRIVER_LIBRARIES AND INDI_CLIENT_LIBRARIES)
+  find_library(INDI_LIBRARIES NAMES indi
+    PATHS
+    ${PC_INDI_LIBRARY_DIRS}
+    ${_obLinkDir}
+    ${GNUWIN32_DIR}/lib
+  )
+
+message("INDI LIB " ${INDI_LIBRARIES})
+
+  find_library(INDI_CLIENT_LIBRARIES NAMES indiclient
+    PATHS
+    ${PC_INDI_LIBRARY_DIRS}
+    ${_obLinkDir}
+    ${GNUWIN32_DIR}/lib
+  )
+
+message("INDI CLIENT LIB " ${INDI_CLIENT_LIBRARIES})
+
+  if (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND INDI_CLIENT_LIBRARIES)
+    set(INDI_FOUND TRUE)
+  else (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND INDI_CLIENT_LIBRARIES)
+    set(INDI_FOUND FALSE)
+  endif (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND INDI_CLIENT_LIBRARIES)
+
+  if (INDI_FOUND)
+    if (NOT INDI_FIND_QUIETLY)
+      message(STATUS "Found INDI: ${INDI_LIBRARIES}, ${INDI_CLIENT_LIBRARIES}, ${INDI_INCLUDE_DIR}")
+    endif (NOT INDI_FIND_QUIETLY)
+  else (INDI_FOUND)
+    if (INDI_FIND_REQUIRED)
+      message(FATAL_ERROR "INDI not found. Please install INDI and try again.")
+    endif (INDI_FIND_REQUIRED)
+  endif (INDI_FOUND)
+
+  mark_as_advanced(INDI_INCLUDE_DIR INDI_LIBRARIES INDI_CLIENT_LIBRARIES)
+
+endif (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND INDI_CLIENT_LIBRARIES)

CMakeLists.txt
@@ -95,8 +95,12 @@ else()
     SET(HAVE_KF5WIT 0)
 endif()
 
-add_definitions(-DQT_USE_FAST_CONCATENATION -DQT_USE_FAST_OPERATOR_PLUS -Dfexceptions)
+add_definitions(-DQT_USE_FAST_CONCATENATION -DQT_USE_FAST_OPERATOR_PLUS)
 add_definitions(-DQT_NO_URL_CAST_FROM_STRING)
+
+if (UNIX)
+add_definitions(-Dfexceptions)
+endif(UNIX)
 
 configure_file(
     ${CMAKE_CURRENT_SOURCE_DIR}/config-kstars.h.cmake

cmake/modules/FindNova.cmake
@@ -1,0 +1,55 @@
+# - Try to find NOVA
+# Once done this will define
+#
+#  NOVA_FOUND - system has NOVA
+#  NOVA_INCLUDE_DIR - the NOVA include directory
+#  NOVA_LIBRARIES - Link these to use NOVA
+
+# Copyright (c) 2006, Jasem Mutlaq <mutlaqja@ikarustech.com>
+# Based on FindLibfacile by Carsten Niehaus, <cniehaus@gmx.de>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+if (NOVA_INCLUDE_DIR AND NOVA_LIBRARIES)
+
+  # in cache already
+  set(NOVA_FOUND TRUE)
+  message(STATUS "Found libnova: ${NOVA_LIBRARIES}")
+
+else (NOVA_INCLUDE_DIR AND NOVA_LIBRARIES)
+
+  find_path(NOVA_INCLUDE_DIR libnova.h
+    PATH_SUFFIXES libnova
+    ${_obIncDir}
+    ${GNUWIN32_DIR}/include
+  )
+
+  find_library(NOVA_LIBRARIES NAMES nova libnova
+    PATHS
+    ${_obLinkDir}
+    ${GNUWIN32_DIR}/lib
+  )
+
+ set(CMAKE_REQUIRED_INCLUDES ${NOVA_INCLUDE_DIR})
+ set(CMAKE_REQUIRED_LIBRARIES ${NOVA_LIBRARIES})
+
+   if(NOVA_INCLUDE_DIR AND NOVA_LIBRARIES)
+    set(NOVA_FOUND TRUE)
+  else (NOVA_INCLUDE_DIR AND NOVA_LIBRARIES)
+    set(NOVA_FOUND FALSE)
+  endif(NOVA_INCLUDE_DIR AND NOVA_LIBRARIES)
+
+  if (NOVA_FOUND)
+    if (NOT Nova_FIND_QUIETLY)
+      message(STATUS "Found NOVA: ${NOVA_LIBRARIES}")
+    endif (NOT Nova_FIND_QUIETLY)
+  else (NOVA_FOUND)
+    if (Nova_FIND_REQUIRED)
+      message(FATAL_ERROR "libnova not found. Please install libnova development package.")
+    endif (Nova_FIND_REQUIRED)
+  endif (NOVA_FOUND)
+
+  mark_as_advanced(NOVA_INCLUDE_DIR NOVA_LIBRARIES)
+  
+endif (NOVA_INCLUDE_DIR AND NOVA_LIBRARIES)

kstars/CMakeLists.txt
@@ -584,13 +584,13 @@ target_link_libraries(KStarsLib
     KF5::KIOFileWidgets
     KF5::WidgetsAddons
     KF5::Plotting
-#    KF5::IconThemes
     Qt5::Gui
     Qt5::PrintSupport
     Qt5::Sql
     Qt5::Svg
     Qt5::Qml
     Qt5::Quick
+    Qt5::Network
     ${ZLIB_LIBRARIES}
 )
 
@@ -603,12 +603,16 @@ if(CFITSIO_FOUND)
 endif(CFITSIO_FOUND)
 
 if(INDI_FOUND)
+    if(WIN32)
+        add_definitions(-DUSE_QT5_INDI)
+        find_package(Nova REQUIRED)
+        target_link_libraries(KStarsLib ${NOVA_LIBRARIES})
+    endif(WIN32)
     target_link_libraries(KStarsLib
         ${CMAKE_THREAD_LIBS_INIT}
         ${INDI_LIBRARIES}
         ${INDI_CLIENT_LIBRARIES}
-        KF5::Notifications
-        z
+        KF5::Notifications        
     )
 endif(INDI_FOUND)
 

kstars/ekos/ekosmanager.cpp
@@ -58,8 +58,8 @@ EkosManager::EkosManager()
     useST4          =false;
     remoteManagerStart=false;
 
-    indiConnectionStatus = STATUS_IDLE;
-    ekosStartingStatus   = STATUS_IDLE;
+    indiConnectionStatus = EKOS_STATUS_IDLE;
+    ekosStartingStatus   = EKOS_STATUS_IDLE;
 
     profileModel = NULL;
 
@@ -754,8 +754,8 @@ void EkosManager::reset()
     weatherProcess = NULL;
     dustCapProcess = NULL;
 
-    ekosStartingStatus  = STATUS_IDLE;
-    indiConnectionStatus= STATUS_IDLE;
+    ekosStartingStatus  = EKOS_STATUS_IDLE;
+    indiConnectionStatus= EKOS_STATUS_IDLE;
 
     connectB->setEnabled(false);
     disconnectB->setEnabled(false);
@@ -767,7 +767,7 @@ void EkosManager::reset()
 
 void EkosManager::processINDI()
 {
-    if (ekosStartingStatus == STATUS_SUCCESS || ekosStartingStatus == STATUS_PENDING)
+    if (ekosStartingStatus == EKOS_STATUS_SUCCESS || ekosStartingStatus == EKOS_STATUS_PENDING)
     {
         stop();
     }
@@ -778,7 +778,7 @@ void EkosManager::processINDI()
 bool EkosManager::stop()
 {
     cleanDevices();
-    ekosStartingStatus = STATUS_IDLE;
+    ekosStartingStatus = EKOS_STATUS_IDLE;
 
     profileGroup->setEnabled(true);
 
@@ -945,13 +945,13 @@ bool EkosManager::start()
             INDIListener::Instance()->disconnect(this);
             qDeleteAll(managedDrivers);
             managedDrivers.clear();
-            ekosStartingStatus = STATUS_ERROR;            
+            ekosStartingStatus = EKOS_STATUS_ERROR;            
             return false;
         }
 
         connect(DriverManager::Instance(), SIGNAL(serverTerminated(QString,QString)), this, SLOT(cleanDevices()));
 
-        ekosStartingStatus = STATUS_PENDING;
+        ekosStartingStatus = EKOS_STATUS_PENDING;
 
         appendLogText(i18n("INDI services started on port %1. Please connect devices.", managedDrivers.first()->getPort()));
 
@@ -996,13 +996,13 @@ bool EkosManager::start()
             INDIListener::Instance()->disconnect(this);
             qDeleteAll(managedDrivers);
             managedDrivers.clear();
-            ekosStartingStatus = STATUS_ERROR;
+            ekosStartingStatus = EKOS_STATUS_ERROR;
             QApplication::restoreOverrideCursor();
             return false;
         }
 
         QApplication::restoreOverrideCursor();
-        ekosStartingStatus = STATUS_PENDING;
+        ekosStartingStatus = EKOS_STATUS_PENDING;
 
         appendLogText(i18n("INDI services started. Connection to remote INDI server is successful. Waiting for devices..."));
 
@@ -1024,12 +1024,12 @@ bool EkosManager::start()
 void EkosManager::checkINDITimeout()
 {
     // Don't check anything unless we're still pending
-    if (ekosStartingStatus != STATUS_PENDING)
+    if (ekosStartingStatus != EKOS_STATUS_PENDING)
         return;
 
     if (nDevices <= 0)
     {
-        ekosStartingStatus = STATUS_SUCCESS;
+        ekosStartingStatus = EKOS_STATUS_SUCCESS;
         return;
     }
 
@@ -1088,13 +1088,13 @@ void EkosManager::checkINDITimeout()
         }
     }
 
-    ekosStartingStatus = STATUS_ERROR;
+    ekosStartingStatus = EKOS_STATUS_ERROR;
 }
 
 void EkosManager::connectDevices()
 {
 
-    indiConnectionStatus = STATUS_PENDING;
+    indiConnectionStatus = EKOS_STATUS_PENDING;
 
     foreach(ISD::GDInterface *device, genericDevices)
         device->Connect();
@@ -1167,7 +1167,7 @@ void EkosManager::processNewDevice(ISD::GDInterface *devInterface)
 
     if (nDevices <= 0)
     {
-        ekosStartingStatus = STATUS_SUCCESS;
+        ekosStartingStatus = EKOS_STATUS_SUCCESS;
 
         if (localMode == false && nDevices == 0)
             appendLogText(i18n("Remote devices established. Please connect devices."));
@@ -1198,7 +1198,7 @@ void EkosManager::deviceConnected()
     ProfileInfo *pi = getCurrentProfile();
     //if (nConnectedDevices == managedDrivers.count() || (nDevices <=0 && nConnectedDevices == nRemoteDevices))
     if (nConnectedDevices >= pi->drivers.count())
-        indiConnectionStatus = STATUS_SUCCESS;    
+        indiConnectionStatus = EKOS_STATUS_SUCCESS;    
 
     ISD::GDInterface *dev = static_cast<ISD::GDInterface *> (sender());
 
@@ -1248,14 +1248,14 @@ void EkosManager::deviceDisconnected()
     if (dev)
     {
         if (dev->getState("CONNECTION") == IPS_ALERT)
-            indiConnectionStatus = STATUS_ERROR;
+            indiConnectionStatus = EKOS_STATUS_ERROR;
         else if (dev->getState("CONNECTION") == IPS_BUSY)
-            indiConnectionStatus = STATUS_PENDING;
+            indiConnectionStatus = EKOS_STATUS_PENDING;
         else
-            indiConnectionStatus = STATUS_IDLE;
+            indiConnectionStatus = EKOS_STATUS_IDLE;
     }
     else
-        indiConnectionStatus = STATUS_IDLE;
+        indiConnectionStatus = EKOS_STATUS_IDLE;
 
     if (Options::verboseLogging())
     {
@@ -1263,7 +1263,7 @@ void EkosManager::deviceDisconnected()
         qDebug() << "Connected Devices: " << nConnectedDevices << " nDevices: " << nDevices;
     }
 
-    if (indiConnectionStatus == STATUS_IDLE)
+    if (indiConnectionStatus == EKOS_STATUS_IDLE)
         nConnectedDevices--;
 
     if (nConnectedDevices < 0)

kstars/ekos/ekosmanager.h
@@ -60,7 +60,7 @@ public:
     EkosManager();
     ~EkosManager();
 
-    typedef enum { STATUS_IDLE, STATUS_PENDING, STATUS_SUCCESS, STATUS_ERROR } CommunicationStatus;
+    typedef enum { EKOS_STATUS_IDLE, EKOS_STATUS_PENDING, EKOS_STATUS_SUCCESS, EKOS_STATUS_ERROR } CommunicationStatus;
 
     void appendLogText(const QString &);
     //void refreshRemoteDrivers();

kstars/ekos/guide.cpp
@@ -10,17 +10,18 @@
 #include "guide.h"
 
 #include <QDateTime>
-#include <unistd.h>
-
-#include "guide/gmath.h"
-#include "guide/guider.h"
-#include "phd2.h"
-
-#include "Options.h"
 
 #include <KMessageBox>
 #include <KLed>
 #include <KLocalizedString>
+
+#include <basedevice.h>
+
+#include "Options.h"
+
+#include "guide/gmath.h"
+#include "guide/guider.h"
+#include "phd2.h"
 
 #include "indi/driverinfo.h"
 #include "indi/clientmanager.h"
@@ -30,8 +31,6 @@
 
 #include "guide/rcalibration.h"
 #include "guideadaptor.h"
-
-#include <basedevice.h>
 
 namespace Ekos
 {

kstars/ekos/guide/rcalibration.cpp
@@ -12,7 +12,6 @@
 #include "rcalibration.h"
 #include "Options.h"
 
-#include <unistd.h>
 #include <time.h>
 #include <assert.h>
 

commit bbd70f646ece7f512eb4964b8884aa399e002ed2

Fix regression where INDI version check is ignored
40cf6ad6cd9a8773b78da828daf98069f4777d76 introduced a regression where any
version of INDI was accepted, even if it did not meet the minimum version
requirement.

REVIEW: 128523

cmake/modules/FindINDI.cmake
@@ -106,7 +106,7 @@ if(ANDROID)
     set(INDI_FOUND FALSE)
   endif(INDI_CLIENT_ANDROID_LIBRARIES)
 else(ANDROID)
-  if (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND (INDI_CLIENT_LIBRARIES OR INDI_CLIENT_QT_LIBRARIES))
+  if (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND (INDI_CLIENT_LIBRARIES OR INDI_CLIENT_QT_LIBRARIES) AND INDI_VERSION_OK)
       # If INDI is found we need to make sure on WIN32 we have INDI Client Qt backend otherwise we can't use INDI
       if (WIN32)
           if (INDI_CLIENT_QT_LIBRARIES)
@@ -117,9 +117,9 @@ else(ANDROID)
       else (WIN32)
           set(INDI_FOUND TRUE)
       endif(WIN32)
-  else (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND (INDI_CLIENT_LIBRARIES OR INDI_CLIENT_QT_LIBRARIES))
+  else (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND (INDI_CLIENT_LIBRARIES OR INDI_CLIENT_QT_LIBRARIES) AND INDI_VERSION_OK)
     set(INDI_FOUND FALSE)
-  endif (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND (INDI_CLIENT_LIBRARIES OR INDI_CLIENT_QT_LIBRARIES))
+  endif (INDI_INCLUDE_DIR AND INDI_LIBRARIES AND (INDI_CLIENT_LIBRARIES OR INDI_CLIENT_QT_LIBRARIES) AND INDI_VERSION_OK)
 endif(ANDROID)
 
   if (INDI_FOUND)