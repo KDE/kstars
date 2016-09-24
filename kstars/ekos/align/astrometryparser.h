f19e281f7d0d928ffe53c9592b46bab173d7a253
Jasem Mutlaq <mutlaqja@ikarustech.com>
2016-09-24 23:14:08 +0300

    Moving all align module files to their own dedicated directly. GUI is simplified even more

---
+++ kstars/ekos/align/astrometryparser.h
@@ -0,0 +1,48 @@
+/*  Astrometry.net Parser
+    Copyright (C) 2012 Jasem Mutlaq <mutlaqja@ikarustech.com>
+
+    This application is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+*/
+
+#ifndef ASTROMETRYPARSER_H
+#define ASTROMETRYPARSER_H
+
+#include <QObject>
+
+namespace Ekos
+{
+
+class Align;
+
+/**
+ * @class AstrometryParser
+ *  AstrometryParser is an interface for online and offline astrometry parsers.
+ *
+ * @authro Jasem Mutlaq
+ */
+
+class AstrometryParser : public QObject
+{
+    Q_OBJECT
+
+public:
+    AstrometryParser();
+    virtual ~AstrometryParser();
+
+    virtual void setAlign(Align *align) = 0;
+    virtual bool init() = 0;
+    virtual void verifyIndexFiles(double fov_x, double fov_y) =0;
+    virtual bool startSovler(const QString &filename, const QStringList &args, bool generated=true) =0;
+    virtual bool stopSolver() = 0;
+
+signals:
+    void solverFinished(double orientation, double ra, double dec, double pixscale);
+    void solverFailed();
+};
+
+}
+
+#endif // ASTROMETRYPARSER_H
