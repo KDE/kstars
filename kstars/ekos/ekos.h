c3db5325ec1be1d2c036d7ad12ae593bf71e5fc3
Jasem Mutlaq <mutlaqja@ikarustech.com>
2017-03-01 13:42:12 +0300

    Move gsl polynomial function to Ekos namespace since is can be used by multiple modules

--- kstars/ekos/ekos.h
+++ kstars/ekos/ekos.h
@@ -1,11 +1,11 @@
 /*  Ekos
-    Copyright (C) 2016 Jasem Mutlaq <mutlaqja@ikarustech.com>
+    Copyright (C) 2017 Jasem Mutlaq <mutlaqja@ikarustech.com>
 
     This application is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
- */
+*/
 
 #ifndef EKOS_H
 #define EKOS_H
@@ -50,6 +50,9 @@ namespace Ekos
     typedef enum { ALIGN_IDLE, ALIGN_COMPLETE, ALIGN_FAILED, ALIGN_ABORTED, ALIGN_PROGRESS, ALIGN_SYNCING, ALIGN_SLEWING} AlignState;
 
     const QString &getAlignStatusString(AlignState state);
+
+    std::vector<double> gsl_polynomial_fit(const double * const data_x, const double * const data_y, const int n, const int order,
+                                           double & chisq);
 }
 
 #endif // EKOS_H
