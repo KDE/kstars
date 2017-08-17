3f8c15bf57b0c8896eac18ab993d43a6087a1efa
Jasem Mutlaq <mutlaqja@ikarustech.com>
2017-08-17 16:22:33 +0300

    Use i18n for polaris to solve issue in non-English locales

--- kstars/tools/polarishourangle.cpp
+++ kstars/tools/polarishourangle.cpp
@@ -34,7 +34,7 @@ PolarisHourAngle::PolarisHourAngle(QWidget *parent) :
   setupUi(this);
   setFixedHeight(size().height());
 
-  SkyObject *polaris = KStarsData::Instance()->skyComposite()->findByName("polaris");
+  SkyObject *polaris = KStarsData::Instance()->skyComposite()->findByName(i18n("Polaris"));
   Q_ASSERT_X(polaris != nullptr, "PolarisHourAngle", "Unable to find Polaris!");
   m_polaris = polaris->clone();
 
