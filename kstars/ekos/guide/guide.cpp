e4185e0ef41adfc54af347441e27559776e4e6ae
Jasem Mutlaq <mutlaqja@ikarustech.com>
2017-10-08 13:42:18 +0300

    Add total RMS to the guide module

--- kstars/ekos/guide/guide.cpp
+++ kstars/ekos/guide/guide.cpp
@@ -2102,6 +2102,7 @@ void Guide::setAxisSigma(double ra, double de)
 {
     l_ErrRA->setText(QString::number(ra, 'f', 2));
     l_ErrDEC->setText(QString::number(de, 'f', 2));
+    l_TotalRMS->setText(QString::number(sqrt(ra*ra+de*de), 'f', 2));
     emit sigmasUpdated(ra, de);
 }
 
