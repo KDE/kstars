8573b262a975a05ccc37c0ff1427415c30c17fab
Jasem Mutlaq <mutlaqja@ikarustech.com>
2017-10-29 09:49:12 +0300

    Check if model is in DSLRInfo

--- kstars/ekos/capture/capture.h
+++ kstars/ekos/capture/capture.h
@@ -524,6 +524,7 @@ class Capture : public QWidget, public Ui::Capture
     // DSLR Info
     void addDSLRInfo(const QString &model, uint32_t maxW, uint32_t maxH, double pixelW, double pixelH);
     void cullToCameraLimits();
+    bool isModelinDSLRInfo(const QString &model);
 
     /* Meridian Flip */
     bool checkMeridianFlip();
