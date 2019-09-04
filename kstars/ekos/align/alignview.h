361a0163efe38fbeae17448507d4b56bbc6ca441
Jasem Mutlaq <mutlaqja@ikarustech.com>
2019-09-04 21:08:06 +0300

    processMarkSelection should be overridden in align view to prevent double clicking from moving the marker by mistake

--- kstars/ekos/align/alignview.h
+++ kstars/ekos/align/alignview.h
@@ -19,33 +19,34 @@ class QPainter;
 
 class AlignView : public FITSView
 {
-    Q_OBJECT
-  public:
-    explicit AlignView(QWidget *parent = nullptr, FITSMode mode = FITS_NORMAL, FITSScale filter = FITS_NONE);
-    ~AlignView() = default;
+        Q_OBJECT
+    public:
+        explicit AlignView(QWidget *parent = nullptr, FITSMode mode = FITS_NORMAL, FITSScale filter = FITS_NONE);
 
-    /* Calculate WCS header info and update WCS info */
-    bool createWCSFile(const QString &newWCSFile, double orientation, double ra, double dec, double pixscale);
+        /* Calculate WCS header info and update WCS info */
+        bool createWCSFile(const QString &newWCSFile, double orientation, double ra, double dec, double pixscale);
 
-    void drawOverlay(QPainter *) override;
+        void drawOverlay(QPainter *) override;
 
-    // Correction line
-    void setCorrectionParams(QLineF &line);
-    void setCorrectionOffset(QPointF &newOffset);
+        // Correction line
+        void setCorrectionParams(QLineF &line);
+        void setCorrectionOffset(QPointF &newOffset);
 
-    void setRACircle(const QVector3D &value);
-    void setRefreshEnabled(bool enable);
+        void setRACircle(const QVector3D &value);
+        void setRefreshEnabled(bool enable);
 
-  protected:
-    void drawLine(QPainter *painter);
-    void drawCircle(QPainter *painter);
+    protected:
+        void drawLine(QPainter *painter);
+        void drawCircle(QPainter *painter);
 
-  private:
-    // Correction Line
-    QLineF correctionLine;
-    QPointF correctionOffset, celestialPolePoint;
-    QVector3D RACircle;
+        virtual void processMarkerSelection(int x, int y) override;
 
-signals:
-    void newCorrectionVector(QLineF correctionVector);
+    private:
+        // Correction Line
+        QLineF correctionLine;
+        QPointF correctionOffset, celestialPolePoint;
+        QVector3D RACircle;
+
+    signals:
+        void newCorrectionVector(QLineF correctionVector);
 };
