1a13ee2eab36b97bb93d7ec47e46fb40401c22e4
Jasem Mutlaq <mutlaqja@ikarustech.com>
2019-11-29 19:55:35 +0300

    Retry connection in case we have a refused connection

--- kstars/ekos/ekoslive/ekosliveclient.h
+++ kstars/ekos/ekoslive/ekosliveclient.h
@@ -34,59 +34,71 @@ namespace EkosLive
 {
 class Client : public QDialog, public Ui::EkosLiveDialog
 {
-    Q_OBJECT
-public:
-    explicit Client(Ekos::Manager *manager);
-    ~Client();
+        Q_OBJECT
+    public:
+        explicit Client(Ekos::Manager *manager);
+        ~Client();
 
-    bool isConnected() const { return m_isConnected; }
+        bool isConnected() const
+        {
+            return m_isConnected;
+        }
 
-    const QPointer<Message> & message() { return m_Message; }
-    const QPointer<Media> & media() { return m_Media; }
-    const QPointer<Cloud> & cloud() { return m_Cloud; }
+        const QPointer<Message> &message()
+        {
+            return m_Message;
+        }
+        const QPointer<Media> &media()
+        {
+            return m_Media;
+        }
+        const QPointer<Cloud> &cloud()
+        {
+            return m_Cloud;
+        }
 
-    void setConnected(bool enabled);
-    void setConfig(bool onlineService, bool rememberCredentials, bool autoConnect);
-    void setUser(const QString &user, const QString &pass);
+        void setConnected(bool enabled);
+        void setConfig(bool onlineService, bool rememberCredentials, bool autoConnect);
+        void setUser(const QString &user, const QString &pass);
 
-signals:
-    void connected();
-    void disconnected();
+    signals:
+        void connected();
+        void disconnected();
 
-protected slots:
-    void authenticate();
-    void onResult(QNetworkReply *reply);
+    protected slots:
+        void authenticate();
+        void onResult(QNetworkReply *reply);
 
-private slots:
-    // Auth Server
-    void connectAuthServer();
-    void disconnectAuthServer();
+    private slots:
+        // Auth Server
+        void connectAuthServer();
+        void disconnectAuthServer();
 
-    void onConnected();
-    void onDisconnected();
+        void onConnected();
+        void onDisconnected();
 
-private:
-    QJsonObject authResponse;
-    QWebSocket m_mediaWebSocket;
-    bool m_isConnected { false };
-    Ekos::Manager *m_Manager { nullptr };
-    QNetworkAccessManager *networkManager { nullptr };
+    private:
+        QJsonObject authResponse;
+        QWebSocket m_mediaWebSocket;
+        bool m_isConnected { false };
+        Ekos::Manager *m_Manager { nullptr };
+        QNetworkAccessManager *networkManager { nullptr };
 
-    QProgressIndicator *pi { nullptr };
+        QProgressIndicator *pi { nullptr };
 
-    QString token;
-    QUrl m_serviceURL;
-    QUrl m_wsURL;
+        QString token;
+        QUrl m_serviceURL;
+        QUrl m_wsURL;
 
-    uint16_t m_MediaReconnectTries {0};
+        uint16_t m_AuthReconnectTries {0};
 
-    QPointer<Message> m_Message;
-    QPointer<Media> m_Media;
-    QPointer<Cloud> m_Cloud;
+        QPointer<Message> m_Message;
+        QPointer<Media> m_Media;
+        QPointer<Cloud> m_Cloud;
 
-    // Retry every 5 seconds in case remote server is down
-    static const uint16_t RECONNECT_INTERVAL = 5000;
-    // Retry for 1 hour before giving up
-    static const uint16_t RECONNECT_MAX_TRIES = 720;
+        // Retry every 3 seconds if connection is refused.
+        static const uint16_t RECONNECT_INTERVAL = 3000;
+        // Retry for 3 times before giving up
+        static const uint16_t RECONNECT_MAX_TRIES = 3;
 };
 }
