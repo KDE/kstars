c041124fa532e011d613855a7ee012f772128ace
Hy Murveit <hy-2@murveit.com>
2020-04-26 14:59:24 +0300

    Improve Overlay Rendering
    Summary:
    Previous PR to reduce fitsview zoom memory use created pixelated rendering for some overlays
    with large amount of zoom--e.g. when using a small view (e.g. around a focus view with subframe enabled.
    Here we employ a different rendering strategy when the view has a smaller number of pixels (and thus
    memory use shouldn't be as much of an issue) so that the rendering is smoother.
    
    Test Plan:
    Load fits images, try all the overlays, zoom in and out.
    Overlays should not change with images greater than a megapixel.
    Try this with various views with subframing (e.g. focus), and the rendering should be less pixelated.
    
    Reviewers: mutlaqja
    
    Reviewed By: mutlaqja
    
    Subscribers: kde-edu
    
    Tags: #kde_edu
    
    Differential Revision: https://phabricator.kde.org/D29192

--- kstars/fitsviewer/fitsview.h
+++ kstars/fitsviewer/fitsview.h
@@ -113,16 +113,17 @@ class FITSView : public QScrollArea
         }
 
         // Overlay
-        virtual void drawOverlay(QPainter *);
+        virtual void drawOverlay(QPainter *, double scale);
 
         // Overlay objects
-        void drawStarCentroid(QPainter *);
-        void drawTrackingBox(QPainter *);
-        void drawMarker(QPainter *);
-        void drawCrosshair(QPainter *);
-        void drawEQGrid(QPainter *);
-        void drawObjectNames(QPainter *painter);
-        void drawPixelGrid(QPainter *painter);
+        void drawStarFilter(QPainter *, double scale);
+        void drawStarCentroid(QPainter *, double scale);
+        void drawTrackingBox(QPainter *, double scale);
+        void drawMarker(QPainter *, double scale);
+        void drawCrosshair(QPainter *, double scale);
+        void drawEQGrid(QPainter *, double scale);
+        void drawObjectNames(QPainter *painter, double scale);
+        void drawPixelGrid(QPainter *painter, double scale);
 
         bool isImageStretched();
         bool isCrosshairShown();
@@ -270,10 +271,12 @@ class FITSView : public QScrollArea
         void calculateMaxPixel(double min, double max);
         void initDisplayImage();
 
-        QPointF getPointForGridLabel(QPainter *painter, const QString& str);
-        bool pointIsInImage(QPointF pt);
+        QPointF getPointForGridLabel(QPainter *painter, const QString& str, double scale);
+        bool pointIsInImage(QPointF pt, double scale);
 
         void loadInFrame();
+
+        double getScale();
 
         /// WCS Future Watcher
         QFutureWatcher<bool> wcsWatcher;
@@ -293,6 +296,11 @@ class FITSView : public QScrollArea
         bool processData();
         void doStretch(FITSData *data, QImage *outputImage);
         double scaleSize(double size);
+        bool isLargeImage();
+        void updateFrameLargeImage();
+        void updateFrameSmallImage();
+        bool drawHFR(QPainter * painter, const QString & hfr, int x, int y);
+
 
         QLabel *noImageLabel { nullptr };
         QPixmap noImage;
