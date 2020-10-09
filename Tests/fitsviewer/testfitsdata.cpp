12ab6230f469354224b5c703cbb0bacf1c22d80c
Jasem Mutlaq <mutlaqja@ikarustech.com>
2020-10-09 22:42:29 +0300

    Fix testfitsdata after latest changes

--- Tests/fitsviewer/testfitsdata.cpp
+++ Tests/fitsviewer/testfitsdata.cpp
@@ -52,14 +52,14 @@ void TestFitsData::testComputeHFR_data()
     // to FITS_FOCUS images, only to FITS_NORMAL images.
 
     // Normal HFR tests
-    QTest::newRow("NGC4535-1-FOCUS") << "ngc4535-autofocus1.fits" << FITS_FOCUS << 12 << 3.67;
-    QTest::newRow("NGC4535-2-FOCUS") << "ngc4535-autofocus2.fits" << FITS_FOCUS << 22 << 1.91;
-    QTest::newRow("NGC4535-3-FOCUS") << "ngc4535-autofocus3.fits" << FITS_FOCUS << 50 << 1.38;
+    QTest::newRow("NGC4535-1-FOCUS") << "ngc4535-autofocus1.fits" << FITS_FOCUS << 11 << 3.88;
+    QTest::newRow("NGC4535-2-FOCUS") << "ngc4535-autofocus2.fits" << FITS_FOCUS << 17 << 2.16;
+    QTest::newRow("NGC4535-3-FOCUS") << "ngc4535-autofocus3.fits" << FITS_FOCUS << 100 << 1.23;
 
     // Focus HFR tests
-    QTest::newRow("NGC4535-1-NORMAL") << "ngc4535-autofocus1.fits" << FITS_NORMAL << 4 << 2.99;
+    QTest::newRow("NGC4535-1-NORMAL") << "ngc4535-autofocus1.fits" << FITS_NORMAL << 4 << 3.04;
     QTest::newRow("NGC4535-2-NORMAL") << "ngc4535-autofocus2.fits" << FITS_NORMAL << 6 << 1.82;
-    QTest::newRow("NGC4535-3-NORMAL") << "ngc4535-autofocus3.fits" << FITS_NORMAL << 40 << 1.17;
+    QTest::newRow("NGC4535-3-NORMAL") << "ngc4535-autofocus3.fits" << FITS_NORMAL << 30 << 1.24;
 #endif
 }
 
@@ -83,10 +83,13 @@ void TestFitsData::testComputeHFR()
     QTRY_VERIFY_WITH_TIMEOUT(worker.isFinished(), 60000);
     QVERIFY(worker.result());
 
-    QCOMPARE(d->findStars(ALGORITHM_SEP), NSTARS);
+    worker = d->findStars(ALGORITHM_SEP);
+    QTRY_VERIFY_WITH_TIMEOUT(worker.isFinished(), 10000);
+    QVERIFY(worker.result());
+
     QCOMPARE(d->getDetectedStars(), NSTARS);
     QCOMPARE(d->getStarCenters().count(), NSTARS);
-    QVERIFY(abs(d->getHFR() - HFR) <= 0.01);
+    QVERIFY(abs(d->getHFR() - HFR) <= 0.1);
 #endif
 }
 
@@ -127,7 +130,7 @@ void TestFitsData::testBahtinovFocusHFR()
     // The bahtinov algorithm depends on which star is selected and number of average rows - not sure how to fiddle with that yet
     const QRect trackingBox(204, 240, 128, 128);
 
-    QCOMPARE(d->findStars(ALGORITHM_BAHTINOV, trackingBox), 1);
+    d->findStars(ALGORITHM_BAHTINOV, trackingBox).waitForFinished();
     QCOMPARE(d->getDetectedStars(), NSTARS);
     QCOMPARE(d->getStarCenters().count(), 1);
     QVERIFY(abs(d->getHFR() - HFR) < 0.01);
@@ -168,11 +171,11 @@ void TestFitsData::initGenericDataFixture()
             << "m47_sim_stars.fits"
             << FITS_NORMAL
             << 80       // Stars found with the Centroid detection
-            << 50       // Stars found with the StellarSolver detection (Quick HFR)
+            << 100      // Stars found with the StellarSolver detection (Quick HFR)
             << 1.49     // HFR found with the Centroid detection
             << 1.80     // HFR found with the Gradient detection
             << 0.0      // HFR found with the Threshold detection - not used
-            << 2.27     // HFR found with the StellarSolver detection
+            << 2.08     // HFR found with the StellarSolver detection
             << 41.08    // ADU
             << 41.08    // Mean
             << 360.18   // StdDev
@@ -244,19 +247,19 @@ void TestFitsData::testLoadFits()
     QCOMPARE(fd->getHFR(), -1.0);
 
     // Default algorithm is centroid, 80 stars with 1.495 as HFR
-    QCOMPARE(fd->findStars(), NSTARS_CENTROID);
+    fd->findStars().waitForFinished();
     QCOMPARE(fd->getDetectedStars(), NSTARS_CENTROID);
     QCOMPARE(fd->getStarCenters().count(), NSTARS_CENTROID);
     QVERIFY(abs(fd->getHFR() - HFR_CENTROID) < 0.01);
 
     // With the centroid algorithm, 80 stars with MEAN HFR 1.495
-    QCOMPARE(fd->findStars(ALGORITHM_CENTROID), NSTARS_CENTROID);
+    fd->findStars(ALGORITHM_CENTROID).waitForFinished();
     QCOMPARE(fd->getDetectedStars(), NSTARS_CENTROID);
     QCOMPARE(fd->getStarCenters().count(), NSTARS_CENTROID);
     QVERIFY(abs(fd->getHFR() - HFR_CENTROID) < 0.01);
 
     // With the gradient algorithm, one single star found with HFR 1.801
-    QCOMPARE(fd->findStars(ALGORITHM_GRADIENT), 1);
+    fd->findStars(ALGORITHM_GRADIENT).waitForFinished();
     QCOMPARE(fd->getDetectedStars(), 1);
     QCOMPARE(fd->getStarCenters().count(), 1);
     QVERIFY(abs(fd->getHFR() - HFR_GRADIENT) < 0.01);
@@ -267,14 +270,14 @@ void TestFitsData::testLoadFits()
     //QCOMPARE(fd->getStarCenters().count(), 0);
     //QCOMPARE(fd->getHFR(), -1.0);
 
-    // With the SEP algorithm, 100 stars with MEAN HFR 2.09
-    QCOMPARE(fd->findStars(ALGORITHM_SEP), NSTARS_STELLARSOLVER);
+    // With the SEP algorithm, 100 stars with MEAN HFR 2.08
+    fd->findStars(ALGORITHM_SEP).waitForFinished();
     QCOMPARE(fd->getDetectedStars(), NSTARS_STELLARSOLVER);
     QCOMPARE(fd->getStarCenters().count(), NSTARS_STELLARSOLVER);
-    QVERIFY(abs(fd->getHFR() - HFR_STELLARSOLVER) < 0.01);
+    QVERIFY(abs(fd->getHFR() - HFR_STELLARSOLVER) < 0.1);
 
     // Test the SEP algorithm with a tracking box, as used by the internal guider and subframe focus.
-    QCOMPARE(fd->findStars(ALGORITHM_SEP, TRACKING_BOX), 1);
+    fd->findStars(ALGORITHM_SEP, TRACKING_BOX).waitForFinished();
     auto centers = fd->getStarCenters();
     QCOMPARE(centers.count(), 1);
     QWARN(QString("Center    %1,%2").arg(centers[0]->x).arg(centers[0]->y).toStdString().c_str());
@@ -310,7 +313,7 @@ void TestFitsData::testCentroidAlgorithmBenchmark()
     QTRY_VERIFY_WITH_TIMEOUT(worker.isFinished(), 10000);
     QVERIFY(worker.result());
 
-    QBENCHMARK { d->findStars(ALGORITHM_CENTROID); }
+    QBENCHMARK { d->findStars(ALGORITHM_CENTROID).waitForFinished(); }
 #endif
 }
 
@@ -340,7 +343,7 @@ void TestFitsData::testGradientAlgorithmBenchmark()
     QTRY_VERIFY_WITH_TIMEOUT(worker.isFinished(), 10000);
     QVERIFY(worker.result());
 
-    QBENCHMARK { d->findStars(ALGORITHM_GRADIENT); }
+    QBENCHMARK { d->findStars(ALGORITHM_GRADIENT).waitForFinished(); }
 #endif
 }
 
@@ -372,7 +375,7 @@ void TestFitsData::testThresholdAlgorithmBenchmark()
     QTRY_VERIFY_WITH_TIMEOUT(worker.isFinished(), 10000);
     QVERIFY(worker.result());
 
-    QBENCHMARK { d->findStars(ALGORITHM_THRESHOLD); }
+    QBENCHMARK { d->findStars(ALGORITHM_THRESHOLD).waitForFinished(); }
 #endif
 }
 
@@ -402,7 +405,7 @@ void TestFitsData::testSEPAlgorithmBenchmark()
     QTRY_VERIFY_WITH_TIMEOUT(worker.isFinished(), 10000);
     QVERIFY(worker.result());
 
-    QBENCHMARK { d->findStars(ALGORITHM_SEP); }
+    QBENCHMARK { d->findStars(ALGORITHM_SEP).waitForFinished(); }
 #endif
 }
 
