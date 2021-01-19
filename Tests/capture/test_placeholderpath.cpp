b4e3509520c18826ae7fc8f8b1cef4b55725ca1f
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2021-01-19 09:41:40 +0100

    Empty TestPlaceholderPath class in preparation of issue #54
    * added empty Tests/capture/test_placeholderpath.h
    * added empty Tests/capture/test_placeholderpath.cpp
    * added Tests/capture/CMakeLists.txt
    * modified Tests/CMakeLists.txt to enable the new test
    * kept empty test testPrecess and empty test fixture testPrecess_data

---
+++ Tests/capture/test_placeholderpath.cpp
@@ -0,0 +1,37 @@
+/***************************************************************************
+                   test_placeholderpath.cpp  -  KStars Planetarium
+                             -------------------
+    begin                : Mon 18 Jan 2021 11:51:21 CDT
+    copyright            : (c) 2021 by Kwon-Young Choi
+    email                : kwon-young.choi@hotmail.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/* Project Includes */
+#include "test_placeholderpath.h"
+
+TestPlaceholderPath::TestPlaceholderPath() : QObject()
+{
+}
+
+TestPlaceholderPath::~TestPlaceholderPath()
+{
+}
+
+void TestPlaceholderPath::testPrecess_data()
+{
+}
+
+void TestPlaceholderPath::testPrecess()
+{
+}
+
+QTEST_GUILESS_MAIN(TestPlaceholderPath)
