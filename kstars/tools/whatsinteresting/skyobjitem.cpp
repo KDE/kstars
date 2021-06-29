f2972aa6f7c87a22e6de7b95078aadbd6d568f88
Valentin Boettcher <hiro@protagon.space>
2021-06-29 10:50:48 +0200

    fix the thumbnails in what's interesting

--- kstars/tools/whatsinteresting/skyobjitem.cpp
+++ kstars/tools/whatsinteresting/skyobjitem.cpp
@@ -106,8 +106,9 @@ void SkyObjItem::setPosition(SkyObject *so)
     else
     {
         KStarsData *data  = KStarsData::Instance();
-        KStarsDateTime ut = data->geo()->LTtoUT(KStarsDateTime(QDateTime::currentDateTime().toLocalTime()));
-        SkyPoint sp       = so->recomputeCoords(ut, data->geo());
+        KStarsDateTime ut = data->geo()->LTtoUT(
+            KStarsDateTime(QDateTime::currentDateTime().toLocalTime()));
+        SkyPoint sp = so->recomputeCoords(ut, data->geo());
 
         //check altitude of object at this time.
         sp.EquatorialToHorizontal(data->lst(), data->geo()->lat());
@@ -119,30 +120,41 @@ void SkyObjItem::setPosition(SkyObject *so)
 
     if (rounded_altitude <= 0)
         m_Position = "<span style='color:red'>" +
-                     xi18n("NOT VISIBLE: About %1 degrees below the %2 horizon", -rounded_altitude,
-                           KSUtils::toDirectionString(azimuth)) +
+                     xi18n("NOT VISIBLE: About %1 degrees below the %2 horizon",
+                           -rounded_altitude, KSUtils::toDirectionString(azimuth)) +
                      "</span>";
     else
         m_Position = "<span style='color:yellow'>" +
-                     xi18n("Now visible: About %1 degrees above the %2 horizon", rounded_altitude,
-                           KSUtils::toDirectionString(azimuth)) +
+                     xi18n("Now visible: About %1 degrees above the %2 horizon",
+                           rounded_altitude, KSUtils::toDirectionString(azimuth)) +
                      "</span>";
 }
 
+QString findImage(const QString &prefix, const SkyObject &obj, const QString &suffix)
+{
+    static const auto base =
+        KSPaths::writableLocation(QStandardPaths::GenericDataLocation);
+    QDirIterator search(
+        base,
+        QStringList() << prefix + obj.name().toLower().remove(' ').remove('/') + suffix,
+        QDir::Files, QDirIterator::Subdirectories);
+
+    return search.hasNext() ? QUrl::fromLocalFile(search.next()).url() : "";
+}
 QString SkyObjItem::getImageURL(bool preferThumb) const
 {
-    QString thumbURL = QUrl::fromLocalFile(KSPaths::locate(QStandardPaths::GenericDataLocation,
-                                                           "thumb-" + m_So->name().toLower().remove(' ') + ".png"))
-                           .url();
-    QString fullSizeURL = QUrl::fromLocalFile(KSPaths::locate(QStandardPaths::GenericDataLocation,
-                                                              "image-" + m_So->name().toLower().remove(' ') + ".png"))
-                              .url();
-    QString wikiImageURL =
+    const auto &thumbURL = findImage("thumb-", *m_So, ".png");
+
+    const auto &fullSizeURL = findImage("image-", *m_So, ".png");
+    const auto &wikiImageURL =
         QUrl::fromLocalFile(KSPaths::locate(QStandardPaths::GenericDataLocation,
-                                            "descriptions/wikiImage-" + m_So->name().toLower().remove(' ') + ".png"))
+                                            "descriptions/wikiImage-" +
+                                                m_So->name().toLower().remove(' ') +
+                                                ".png"))
             .url();
     QString XPlanetURL =
-        QUrl::fromLocalFile(KSPaths::locate(QStandardPaths::GenericDataLocation, "xplanet/" + m_So->name() + ".png"))
+        QUrl::fromLocalFile(KSPaths::locate(QStandardPaths::GenericDataLocation,
+                                            "xplanet/" + m_So->name() + ".png"))
             .url();
 
     //First try to return the preferred file
