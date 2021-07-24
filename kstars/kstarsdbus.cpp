e7a77da01335ac53a8740ac23b6d5ca4d7ec4285
Akarsh Simha <akarsh@kde.org>
2021-07-24 00:57:34 -0700

    Allow access to the internet resolver from DBus. This will allow for more powerful scripting.

--- kstars/kstarsdbus.cpp
+++ kstars/kstarsdbus.cpp
@@ -34,6 +34,8 @@
 #include "skyobjects/ksplanetbase.h"
 #include "skyobjects/starobject.h"
 #include "tools/whatsinteresting/wiview.h"
+#include "dialogs/finddialog.h"
+#include "tools/nameresolver.h"
 
 #ifdef HAVE_CFITSIO
 #include "fitsviewer/fitsviewer.h"
@@ -720,9 +722,28 @@ QString KStars::getDSSURL(double RA_J2000, double Dec_J2000, float width, float 
     return KSDssDownloader::getDSSURL(ra, dec, width, height);
 }
 
-QString KStars::getObjectDataXML(const QString &objectName)
+QString KStars::getObjectDataXML(const QString &objectName, bool fallbackToInternet, bool storeInternetResolved)
 {
-    SkyObject *target = data()->objectNamed(objectName);
+    bool deleteTargetAfterUse = false;
+    const SkyObject *target = data()->objectNamed(objectName);
+    if (!target && fallbackToInternet)
+    {
+        if (!storeInternetResolved)
+        {
+            const auto &cedata = NameResolver::resolveName(objectName);
+            if (cedata.first)
+            {
+                target = cedata.second.clone(); // We have to free this since we own the pointer
+                deleteTargetAfterUse = true; // so note that down
+            }
+        }
+        else
+        {
+            CatalogsDB::DBManager db_manager { CatalogsDB::dso_db_path() };
+            target = FindDialog::resolveAndAdd(db_manager, objectName);
+        }
+
+    }
     if (!target)
     {
         return QString("<xml></xml>");
@@ -749,8 +770,8 @@ QString KStars::getObjectDataXML(const QString &objectName)
     stream.writeTextElement("Type", target->typeName());
     stream.writeTextElement("Magnitude", QString::number(target->mag(), 'g', 2));
     stream.writeTextElement("Position_Angle", QString::number(target->pa(), 'g', 3));
-    StarObject *star   = dynamic_cast<StarObject *>(target);
-    auto *dso = dynamic_cast<CatalogObject *>(target);
+    auto *star = dynamic_cast<const StarObject *>(target);
+    auto *dso = dynamic_cast<const CatalogObject *>(target);
     if (star)
     {
         stream.writeTextElement("Spectral_Type", star->sptype());
@@ -772,6 +793,13 @@ QString KStars::getObjectDataXML(const QString &objectName)
     }
     stream.writeEndElement(); // object
     stream.writeEndDocument();
+
+    if (deleteTargetAfterUse)
+    {
+        Q_ASSERT(!!target);
+        delete target;
+    }
+
     return output;
 }
 
