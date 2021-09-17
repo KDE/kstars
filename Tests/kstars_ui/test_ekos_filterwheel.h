a26205ae97f111b71ee6ae5ca86b96f8cf2800c3
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-17 13:00:15 +0200

    Convert GPL-2.0-or-later copyright statements to SPDX

--- Tests/kstars_ui/test_ekos_filterwheel.h
+++ Tests/kstars_ui/test_ekos_filterwheel.h
@@ -2,10 +2,7 @@
     Copyright (C) 2020
     Eric Dejouhanet <eric.dejouhanet@gmail.com>
 
-    This application is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+    SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 #ifndef TestEkosFilterWheel_H
--- kstars/ekos/guide/internalguide/guidealgorithms.cpp
+++ kstars/ekos/guide/internalguide/guidealgorithms.cpp
@@ -1,12 +1,10 @@
 /*  Algorithms used in the internal guider.
     Copyright (C) 2012 Andrew Stepanenko
 
-    Modified by Jasem Mutlaq <mutlaqja@ikarustech.com> for KStars.
+    Modified by Jasem Mutlaq for KStars.
+    SPDX-FileCopyrightText: Jasem Mutlaq <mutlaqja@ikarustech.com>
 
-    This application is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+    SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 #include "guidealgorithms.h"
