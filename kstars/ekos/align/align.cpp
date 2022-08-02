db028ba4cb7e1acb3afa843d068c77bbfa79cec9
Hy Murveit <hy-2@murveit.com>
2022-08-02 15:14:07 +0000

    fix align solver disconnect crash, disable ssolver logging
    Fixes crash reported by @alacant
    Suggest cherrypicking into 3.6.0

--- kstars/ekos/align/align.cpp
+++ kstars/ekos/align/align.cpp
@@ -417,6 +417,9 @@ Align::Align(ProfileInfo *activeProfile) : m_ActiveProfile(activeProfile)
 
 Align::~Align()
 {
+    if (m_StellarSolver.get() != nullptr)
+        disconnect(m_StellarSolver.get(), &StellarSolver::logOutput, this, &Align::appendLogText);
+
     if (alignWidget->parent() == nullptr)
         toggleAlignWidgetFullScreen();
 
@@ -2028,8 +2031,11 @@ void Align::startSolving()
 
         if(Options::alignmentLogging())
         {
-            m_StellarSolver->setLogLevel(static_cast<SSolver::logging_level>(Options::loggerLevel()));
-            m_StellarSolver->setSSLogLevel(SSolver::LOG_NORMAL);
+            // Not trusting SSolver logging right now (Hy Aug 1, 2021)
+            // m_StellarSolver->setLogLevel(static_cast<SSolver::logging_level>(Options::loggerLevel()));
+            // m_StellarSolver->setSSLogLevel(SSolver::LOG_NORMAL);
+            m_StellarSolver->setLogLevel(SSolver::LOG_NONE);
+            m_StellarSolver->setSSLogLevel(SSolver::LOG_OFF);
             if(Options::astrometryLogToFile())
             {
                 m_StellarSolver->setProperty("LogToFile", true);
