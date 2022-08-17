644c71c839a229e1aaf5f051a300e1d896b88001
Sophie Taylor <sophie@spacekitteh.moe>
2022-08-17 06:07:19 +0000

    Compress saved autofocus and guide images

--- kstars/ekos/guide/internalguide/internalguider.cpp
+++ kstars/ekos/guide/internalguide/internalguider.cpp
@@ -592,7 +592,7 @@ void InternalGuider::setImageData(const QSharedPointer<FITSData> &data)
         // IS8601 contains colons but they are illegal under Windows OS, so replacing them with '-'
         // The timestamp is no longer ISO8601 but it should solve interoperality issues between different OS hosts
         QString name     = "guide_frame_" + now.toString("HH-mm-ss") + ".fits";
-        QString filename = path + QStringLiteral("/") + name;
+        QString filename = path + QStringLiteral("/") + name + QStringLiteral("[compress R 100,100]");
         m_ImageData->saveImage(filename);
     }
 }
