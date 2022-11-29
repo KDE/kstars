bbbe27b654884df34e6f74457fb08ab97307c51f
Jasem Mutlaq <mutlaqja@ikarustech.com>
2022-11-29 08:39:12 +0300

    Fix setAllSettings not saving to KStars options file

--- kstars/ekos/align/align.cpp
+++ kstars/ekos/align/align.cpp
@@ -4145,7 +4145,23 @@ void Align::setAllSettings(const QVariantMap &settings)
         }
     }
 
-    m_Settings = settings;
+    // Sync to options
+    for (auto &key : settings.keys())
+    {
+        auto value = settings[key];
+        // Save immediately
+        Options::self()->setProperty(key.toLatin1(), value);
+        Options::self()->save();
+
+        m_Settings[key] = value;
+        m_GlobalSettings[key] = value;
+    }
+
+    emit settingsUpdated(getAllSettings());
+
+    // Save to optical train specific settings as well
+    OpticalTrainSettings::Instance()->setOpticalTrainID(OpticalTrainManager::Instance()->id(opticalTrainCombo->currentText()));
+    OpticalTrainSettings::Instance()->setOneSetting(OpticalTrainSettings::Align, m_Settings);
 
     // Restablish connections
     connectSettings();
