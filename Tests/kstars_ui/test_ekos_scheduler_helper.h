3714e69031a1a6470184833f1af2eb5b42137f63
Hy Murveit <hy-2@murveit.com>
2022-12-23 17:18:08 -0800

    fix test_ekos_scheduler_ops and make it stable

--- Tests/kstars_ui/test_ekos_scheduler_helper.h
+++ Tests/kstars_ui/test_ekos_scheduler_helper.h
@@ -53,7 +53,7 @@ class TestEkosSchedulerHelper : public TestEkosHelper
         static QString getSchedulerFile(const SkyObject *targetObject, const StartupCondition &startupCondition,
                                         const CompletionCondition &completionCondition, ScheduleSteps steps,
                                         bool enforceTwilight, bool enforceArtificialHorizon, int minAltitude = 30, QString fitsFile = nullptr,
-                                        ShutdownProcedure shutdownProcedure = {false, false, true, false});
+                                        ShutdownProcedure shutdownProcedure = {false, false, true, false}, int errorDelay = 0);
 
         // This is a capture sequence file needed to start up the scheduler. Most fields are ignored by the scheduler,
         // and by the Mock capture module as well.
