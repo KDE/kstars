f063222a643926d196e8bc3167998b08da1f6142
Hy Murveit <hy-2@murveit.com>
2023-05-13 10:43:27 -0700

    Revert compression of saved focus and guide fits files.

--- kstars/ekos/focus/focus.cpp
+++ kstars/ekos/focus/focus.cpp
@@ -2747,8 +2747,8 @@ void Focus::setHFRComplete()
         dir.mkpath(path);
         // IS8601 contains colons but they are illegal under Windows OS, so replacing them with '-'
         // The timestamp is no longer ISO8601 but it should solve interoperality issues between different OS hosts
-        QString name     = "autofocus_frame_" + now.toString("HH-mm-ss") + ".fits.gz";
-        QString filename = path + QStringLiteral("/") + name + QStringLiteral("[compress R 100,100]");
+        QString name     = "autofocus_frame_" + now.toString("HH-mm-ss") + ".fits";
+        QString filename = path + QStringLiteral("/") + name;
         m_ImageData->saveImage(filename);
     }
 
