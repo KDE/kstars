KDE/kstars

commit 5f3dcf74ed053c2a0137729804452209bfb75fe2

Fix FITSViewer not being removed from list after closing window

kstars/kstars.cpp
@@ -598,9 +598,13 @@ const QSharedPointer<FITSViewer> &KStars::createFITSViewer()
     {
         QSharedPointer<FITSViewer> newFITSViewer(new FITSViewer(Options::independentWindowFITS() ? nullptr : KStars::Instance()));
         m_FITSViewers.append(newFITSViewer);
-        connect(newFITSViewer.get(), &FITSViewer::terminated, this, [ & ]()
+        connect(newFITSViewer.get(), &FITSViewer::terminated, this, [this]()
         {
-            m_FITSViewers.removeOne(newFITSViewer);
+            auto rawPointer = dynamic_cast<FITSViewer*>(sender());
+            m_FITSViewers.erase(std::remove_if(m_FITSViewers.begin(), m_FITSViewers.end(), [rawPointer](auto & viewer)
+            {
+                return viewer.get() == rawPointer;
+            }));
         });
         return m_FITSViewers.constLast();
     }

commit ada76a181f1957535748892447f59553227400af

Fix bug introduced in 5f3dcf74

kstars/kstars.cpp
@@ -603,7 +603,7 @@ const QSharedPointer<FITSViewer> &KStars::createFITSViewer()
             auto rawPointer = dynamic_cast<FITSViewer*>(sender());
             m_FITSViewers.erase(std::remove_if(m_FITSViewers.begin(), m_FITSViewers.end(), [rawPointer](auto & viewer)
             {
-                return viewer.get() == rawPointer;
+                return viewer.get() != rawPointer;
             }));
         });
         return m_FITSViewers.constLast();