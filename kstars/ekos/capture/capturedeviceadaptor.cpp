0a0d123d815ecc0ac75cd2694f2d08cc7f750465
Yuri Chornoivan <yurchor@ukr.net>
2025-03-21 09:27:42 +0200

    Fix minor typo

--- kstars/ekos/capture/capturedeviceadaptor.cpp
+++ kstars/ekos/capture/capturedeviceadaptor.cpp
@@ -418,7 +418,7 @@ void Ekos::CaptureDeviceAdaptor::updateFilterPosition()
 {
     if (m_FilterManager.isNull())
     {
-        qCritical(KSTARS_EKOS_CAPTURE) << "Filter manager is not initilized yet. Filter wheel missing from train?";
+        qCritical(KSTARS_EKOS_CAPTURE) << "Filter manager is not initialized yet. Filter wheel missing from train?";
         KSNotification::event(QLatin1String("CaptureFailed"), i18n("Filter manager is not initilized yet. Filter wheel missing from train?"), KSNotification::Capture, KSNotification::Alert);
         emit filterIdChanged(-1);
     }
